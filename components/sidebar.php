5b95c865d23063e7a39a99d1049fec738d222242
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-04-27 12:56:06 +0800

    edited and added new links

--- components/sidebar.php
+++ components/sidebar.php
@@ -2,29 +2,22 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="homepage.php">
         <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-laugh-wink"></i>
+            <!-- <i class="fas fa-laugh-wink"></i> -->
         </div>
-        <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+        <div class="sidebar-brand-text mx-3">Public Health Disease Geomapping</div>
     </a>
 
     <!-- Divider -->
     <hr class="sidebar-divider my-0">
 
-    <!-- Nav Item - Dashboard -->
+    <!-- Nav Item - Dashboard
     <li class="nav-item active">
         <a class="nav-link" href="/admin2gh/homepage.php">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span></a>
-    </li>
-
-    <!-- Nav Item - Charts -->
-    <li class="nav-item">
-        <a class="nav-link" href="http://localhost/admin2gh/form.php">
-            <i class="fas fa-fw fa-chart-area"></i>
-            <span>Charts</span></a>
-    </li>
+    </li> -->
 
     <!-- Nav Item - Tables -->
     <li class="nav-item">
@@ -38,6 +31,20 @@
         <a class="nav-link" href="http://localhost/admin2gh/patientTable.php">
             <i class="fas fa-fw fa-table"></i>
             <span>Patient Tables</span></a>
+    </li>
+
+    <!-- Nav Item - Charts -->
+    <li class="nav-item">
+        <a class="nav-link" href="http://localhost/admin2gh/charts.php">
+            <i class="fas fa-fw fa-chart-area"></i>
+            <span>Charts</span></a>
+    </li>
+
+    <!-- Nav Item - Charts -->
+    <li class="nav-item">
+        <a class="nav-link" href="http://localhost/admin2gh/map.php">
+            <i class="fas fa-fw fa-map"></i>
+            <span>Map</span></a>
     </li>
 
     <!-- Sidebar Toggler (Sidebar) -->
