b7a461520c3904db455b9ef85864d2081132d83a
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-04-27 12:56:14 +0800

    created table for admins

---
+++ components/tableAdmins.php
@@ -0,0 +1,81 @@
+<?php
+include('connection.php');
+?>
+
+<!-- DataTales Example -->
+<div class="card shadow mb-4">
+    <div class="card-header py-3">
+        <div class="row">
+            <h2 class="m-0 font-weight-bold text-primary">
+                Admin's Data
+            </h2>
+            <a href="/phpsandbox/publichealth/createAdmin.php" class="btn btn-primary" role="button">Add new Admin</a>
+        </div>
+    </div>
+
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Contact Number</th>
+                        <th>Address</th>
+                        <th>Barangay</th>
+                        <th>Municipality</th>
+                        <th>Created At</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tfoot>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Contact Number</th>
+                        <th>Address</th>
+                        <th>Barangay</th>
+                        <th>Municipality</th>
+                        <th>Created At</th>
+                        <th>Action</th>
+                    </tr>
+                </tfoot>
+                <tbody>
+                    <?php
+                    // read all the data from db table
+                    $sql = "SELECT clients.*, barangay.barangay, municipality.municipality
+                    FROM clients
+                    LEFT JOIN barangay ON clients.barangay = barangay.id 
+                    LEFT JOIN municipality ON clients.municipality = municipality.munId
+                    ";
+                    // LIMIT $startRecord, $recordsPerPage
+                    $result = $con->query($sql);
+
+                    // check if there is data in the table
+                    if (!$result) {
+                        die('Invalid Query: ' . $con->error);
+                    }
+
+                    while ($row = $result->fetch_object()) {
+                        echo "
+                        <tr>
+                        <td>$row->name</td>
+                        <td>$row->email</td>
+                        <td>$row->contact_number</td>
+                        <td>$row->address</td>
+                        <td>$row->barangay</td>
+                        <td>$row->municipality</td>
+                        <td>$row->created_at</td>
+                        <td>
+                            <a class='btn btn-primary btn-sm' href='/phpsandbox/publichealth/editAdmin.php?id=$row->id'>Edit</a>
+                            <a class='btn btn-danger btn-sm' href='/phpsandbox/publichealth/deleteAdmin.php?id=$row->id'>Remove</a>
+                        </td>
+                        </tr>
+                    ";
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
