c625047c7d23e9a6ed2dd8ad6ae5423a1c6522fc
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-04-27 12:54:18 +0800

    created barchart.php

---
+++ components/barchart.php
@@ -0,0 +1,21 @@
+<div class="card shadow mb-4">
+    <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">Bar Chart</h6>
+    </div>
+    <div class="card-body">
+        <div class="chart-bar">
+            <div class="chartjs-size-monitor">
+                <div class="chartjs-size-monitor-expand">
+                    <div class=""></div>
+                </div>
+                <div class="chartjs-size-monitor-shrink">
+                    <div class=""></div>
+                </div>
+            </div>
+            <canvas id="myBarChart" style="display: block; width: 1039px; height: 320px;" width="1039" height="320" class="chartjs-render-monitor"></canvas>
+        </div>
+        <hr>
+        Styling for the bar chart can be found in the
+        <code>/js/demo/chart-bar-demo.js</code> file.
+    </div>
+</div>
