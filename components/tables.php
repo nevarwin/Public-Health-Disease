c32327557200d20beab0744f62ae81ff5a6fc923
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-04-27 12:56:41 +0800

    edited the table for admins

--- components/tables.php
+++ components/tables.php
@@ -1,3 +1,7 @@
+<?php
+include('connection.php');
+?>
+
 <!-- DataTales Example -->
 <div class="card shadow mb-4">
     <div class="card-header py-3">
@@ -11,56 +15,61 @@
                 <thead>
                     <tr>
                         <th>Name</th>
-                        <th>Position</th>
-                        <th>Office</th>
-                        <th>Age</th>
-                        <th>Start date</th>
-                        <th>Salary</th>
+                        <th>Email</th>
+                        <th>Contact Number</th>
+                        <th>Address</th>
+                        <th>Barangay</th>
+                        <th>Municipality</th>
+                        <th>Created At</th>
+                        <th>Action</th>
                     </tr>
                 </thead>
                 <tfoot>
                     <tr>
                         <th>Name</th>
-                        <th>Position</th>
-                        <th>Office</th>
-                        <th>Age</th>
-                        <th>Start date</th>
-                        <th>Salary</th>
+                        <th>Email</th>
+                        <th>Contact Number</th>
+                        <th>Address</th>
+                        <th>Barangay</th>
+                        <th>Municipality</th>
+                        <th>Created At</th>
+                        <th>Action</th>
                     </tr>
                 </tfoot>
                 <tbody>
-                    <tr>
-                        <td>Tiger Nixon</td>
-                        <td>System Architect</td>
-                        <td>Edinburgh</td>
-                        <td>61</td>
-                        <td>2011/04/25</td>
-                        <td>$320,800</td>
-                    </tr>
-                    <tr>
-                        <td>Garrett Winters</td>
-                        <td>Accountant</td>
-                        <td>Tokyo</td>
-                        <td>63</td>
-                        <td>2011/07/25</td>
-                        <td>$170,750</td>
-                    </tr>
-                    <tr>
-                        <td>Ashton Cox</td>
-                        <td>Junior Technical Author</td>
-                        <td>San Francisco</td>
-                        <td>66</td>
-                        <td>2009/01/12</td>
-                        <td>$86,000</td>
-                    </tr>
-                    <tr>
-                        <td>Cedric Kelly</td>
-                        <td>Senior Javascript Developer</td>
-                        <td>Edinburgh</td>
-                        <td>22</td>
-                        <td>2012/03/29</td>
-                        <td>$433,060</td>
-                    </tr>
+                    <?php
+                    // read all the data from db table
+                    $sql = "SELECT clients.*, barangay.barangay, municipality.municipality
+                    FROM clients
+                    LEFT JOIN barangay ON clients.barangay = barangay.id 
+                    LEFT JOIN municipality ON clients.municipality = municipality.munId
+                    ";
+                    // LIMIT $startRecord, $recordsPerPage
+                    $result = $con->query($sql);
+
+                    // check if there is data in the table
+                    if (!$result) {
+                        die('Invalid Query: ' . $con->error);
+                    }
+
+                    while ($row = $result->fetch_object()) {
+                        echo "
+                        <tr>
+                        <td>$row->name</td>
+                        <td>$row->email</td>
+                        <td>$row->contact_number</td>
+                        <td>$row->address</td>
+                        <td>$row->barangay</td>
+                        <td>$row->municipality</td>
+                        <td>$row->created_at</td>
+                        <td>
+                            <a class='btn btn-primary btn-sm' href='/phpsandbox/publichealth/editAdmin.php?id=$row->id'>Edit</a>
+                            <a class='btn btn-danger btn-sm' href='/phpsandbox/publichealth/deleteAdmin.php?id=$row->id'>Delete</a>
+                        </td>
+                        </tr>
+                    ";
+                    }
+                    ?>
                 </tbody>
             </table>
         </div>
