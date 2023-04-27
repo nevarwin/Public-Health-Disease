27094f347aff34d9d18fcfef46478d1d21c24aaa
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-04-27 12:55:23 +0800

    edited the linechart title

--- components/linechart.php
+++ components/linechart.php
@@ -3,8 +3,8 @@
     <div class="card shadow mb-4">
         <!-- Card Header - Dropdown -->
         <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-            <h6 class="m-0 font-weight-bold text-primary">Earnings Overview</h6>
-            <div class="dropdown no-arrow">
+            <h6 class="m-0 font-weight-bold text-primary">Yearly Disease Cases</h6>
+            <!-- <div class="dropdown no-arrow">
                 <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
                 </a>
@@ -15,7 +15,7 @@
                     <div class="dropdown-divider"></div>
                     <a class="dropdown-item" href="#">Something else here</a>
                 </div>
-            </div>
+            </div> -->
         </div>
         <!-- Card Body -->
         <div class="card-body">
@@ -25,3 +25,7 @@
         </div>
     </div>
 </div>
+
+<?php
+include('script.php');
+?>
