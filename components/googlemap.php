bcc6e7ba9789cac5dd07fbf6e40eb2f471c27be1
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-04-27 12:54:47 +0800

    created googlemap.php

---
+++ components/googlemap.php
@@ -0,0 +1,10 @@
+<!--Google map-->
+<!-- <div id="map-container-google-3" class="z-depth-1-full map-container-3 position-fixed w-100">
+    <iframe src="https://maps.google.com/maps?q=manila&t=k&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" style="border:0" allowfullscreen title="googlemap"></iframe>
+</div> -->
+
+<div class="container-fluid">
+    <div class="embed-responsive embed-responsive-16by9">
+        <iframe class="embed-responsive-item" src="https://maps.google.com/maps?q=manila&t=k&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" style="border:0" allowfullscreen title="googlemap"></iframe>
+    </div>
+</div>
