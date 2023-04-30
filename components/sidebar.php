686d9df49e5e72227c4e2dd2e189fc8cb89cfb22
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-04-30 14:22:55 +0800

    edited sidebar logo

--- components/sidebar.php
+++ components/sidebar.php
@@ -6,7 +6,10 @@
         <div class="sidebar-brand-icon rotate-n-15">
             <!-- <i class="fas fa-laugh-wink"></i> -->
         </div>
-        <div class="sidebar-brand-text mx-3">Public Health Disease Geomapping</div>
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-1">Public Health Disease Geomapping</div>
     </a>
 
     <!-- Divider -->
