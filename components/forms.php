e3ac29cb2a252bf3fc5650fe604d3994cb36135e
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-05-01 00:48:35 +0800

    created a sample form

---
+++ components/forms.php
@@ -0,0 +1,94 @@
+<div class="pagetitle">
+    <h1>Form Layouts</h1>
+    <section class="section">
+        <div class="lg-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Floating labels Form</h5>
+                    <!-- Floating Labels Form -->
+                    <form class="row g-3">
+                        <div class="col-md-4">
+                            <div class="form-floating">
+                                <label for="floatingName">Your Name</label>
+                                <input type="text" class="form-control" id="floatingName" placeholder="Your Name">
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-floating">
+                                <label for="floatingName">Your Name</label>
+                                <input type="text" class="form-control" id="floatingName" placeholder="Your Name">
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-floating">
+                                <label for="floatingName">Your Name</label>
+                                <input type="text" class="form-control" id="floatingName" placeholder="Your Name">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-floating">
+                                <label for="floatingEmail">Your Email</label>
+                                <input type="email" class="form-control" id="floatingEmail" placeholder="Your Email">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-floating">
+                                <label for="floatingPassword">Password</label>
+                                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="col-md-12">
+                                <div class="form-floating">
+                                    <label for="floatingCity">City</label>
+                                    <input type="text" class="form-control" id="floatingCity" placeholder="City">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="col-md-12">
+                                <div class="form-floating">
+                                    <label for="floatingCity">City</label>
+                                    <input type="text" class="form-control" id="floatingCity" placeholder="City">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-floating mb-3">
+                                <label for="floatingSelect">State</label>
+                                <select class="form-select" id="floatingSelect" aria-label="State">
+                                    <option selected>New York</option>
+                                    <option value="1">Oregon</option>
+                                    <option value="2">DC</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-2 ">
+                            <div class="form-floating">
+                                <label for="floatingZip">Zip</label>
+                                <input type="text" class="form-control" id="floatingZip" placeholder="Zip">
+                            </div>
+                        </div>
+                        <div class="container">
+                            <select class="form-control col-md-4 p4" apperance: none>
+                                <option value="">Select a disease</option>
+                                <option value="cancer">Cancer</option>
+                                <option value="diabetes">Diabetes</option>
+                                <option value="heart-disease">Heart Disease</option>
+                                <option value="alzheimer">Alzheimer's Disease</option>
+                                <option value="asthma">Asthma</option>
+                                <option value="flu">Flu</option>
+                            </select>
+                        </div>
+
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                            <button type="reset" class="btn btn-secondary">Reset</button>
+                        </div>
+                    </form>
+                    <!-- End floating Labels Form -->
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
