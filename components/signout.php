76fdfb7578a884a49f9b7291ed5169a5fca7fcf8
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-05-01 14:31:29 +0800

    created signout

---
+++ components/signout.php
@@ -0,0 +1,13 @@
+<?php
+// start the session
+session_start();
+
+// unset all session variables
+$_SESSION = array();
+
+// destroy the session
+session_destroy();
+
+// redirect to the login page
+header("Location: http://localhost/admin2gh/loginForm.php");
+exit;
