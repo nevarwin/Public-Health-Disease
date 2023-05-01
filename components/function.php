bc60fc43cdcceff7a6a7874230fd49c7e11dc82d
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-05-01 14:31:53 +0800

    edited pathing

--- components/function.php
+++ components/function.php
@@ -12,6 +12,6 @@ function check_login($con) {
     }
 
     //redirect to login
-    header('location: http://localhost/admin2gh/adminTable.php');
-    die;
+    header('Location: http://localhost/admin2gh/adminTable.php');
+    exit();
 }
--- components/topbar.php
+++ components/topbar.php
@@ -5,7 +5,6 @@
     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
         <i class="fa fa-bars"></i>
     </button>
-
     <!-- Topbar Search -->
     <!-- <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
         <div class="input-group">
@@ -31,17 +30,17 @@
         <!-- Nav Item - User Information -->
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Raven Solis</span>
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $user_data['name']; ?></span>
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">(Super Admin)</span>
                 <img class="img-profile rounded-circle" src="img/undraw_profile.svg" />
             </a>
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="#">
+                <a class="dropdown-item" href="http://localhost/admin2gh/adminPage-update.php?id=<?= $user_data['id']; ?>">
                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                     Profile
                 </a>
-                <a class="dropdown-item" href="#">
+                <a class="dropdown-item" href="http://localhost/admin2gh/adminPage-update.php?id=<?= $user_data['id']; ?>">
                     <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
                     Settings
                 </a>
@@ -50,7 +49,7 @@
                     Activity Log
                 </a> -->
                 <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                <a class="dropdown-item" href="logoutmodal.php" data-toggle="modal" data-target="#logoutModal">
                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                     Logout
                 </a>
