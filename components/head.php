9b907090a10c391fe7a68d67d42c3758c07adbb1
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-05-01 14:32:24 +0800

    added checklogin to prevent unauthorize access

--- components/head.php
+++ components/head.php
@@ -1,5 +1,12 @@
-<head>
+<?php
+session_start();
+include("./components/connection.php");
+include("./components/function.php");
 
+$user_data = check_login($con);
+?>
+
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
