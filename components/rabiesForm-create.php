76891751927f070ccd82ef46cb448103a31add56
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-05-08 23:43:54 +0800

    created rabies form create

---
+++ components/rabiesForm-create.php
@@ -0,0 +1,80 @@
+<div class="row mb-3">
+  <label for="" class="col-sm-3 col-form-label">Type Of Exposure</label>
+  <div class="col-sm-6">
+    <input type="text" class="form-control" name="typeOfExposure" value="<?php echo $typeOfExposure; ?>" />
+  </div>
+</div>
+<div class="row mb-3">
+  <label for="" class="col-sm-3 col-form-label">Category</label>
+  <div class="col-sm-6">
+    <input type="text" class="form-control" name="category" value="<?php echo $category; ?>" />
+  </div>
+</div>
+<div class="row mb-3">
+  <label for="" class="col-sm-3 col-form-label">Bite Site</label>
+  <div class="col-sm-6">
+    <input type="text" class="form-control" name="biteSite" value="<?php echo $biteSite; ?>" />
+  </div>
+</div>
+<div class="row mb-3">
+  <label for="" class="col-sm-3 col-form-label">Date Bitten</label>
+  <div class="col-sm-6">
+    <input type="date" class="form-control" name="dateBitten" value="<?php echo $dateBitten; ?>" />
+  </div>
+</div>
+<div class="row mb-3">
+  <label for="" class="col-sm-3 col-form-label">Type Of Animal</label>
+  <div class="col-sm-6">
+    <input type="text" class="form-control" name="typeOfAnimal" value="<?php echo $typeOfAnimal; ?>" />
+  </div>
+</div>
+<div class="row mb-3">
+  <label for="" class="col-sm-3 col-form-label">Animal Status</label>
+  <div class="col-sm-6">
+    <input type="text" class="form-control" name="animalStatus" value="<?php echo $animalStatus; ?>" />
+  </div>
+</div>
+<div class="row mb-3">
+  <label for="" class="col-sm-3 col-form-label">Date Vacc Started</label>
+  <div class="col-sm-6">
+    <input type="date" class="form-control" name="dateVaccStarted" value="<?php echo $dateVaccStarted; ?>" />
+  </div>
+</div>
+<div class="row mb-3">
+  <label for="" class="col-sm-3 col-form-label">Animal Vaccination</label>
+  <div class="col-sm-6">
+    <input type="text" class="form-control" name="animalVacc" value="<?php echo $animalVacc; ?>" />
+  </div>
+</div>
+<div class="row mb-3">
+  <label for="" class="col-sm-3 col-form-label">Wound Cleaned</label>
+  <div class="col-sm-6">
+    <input type="text" class="form-control" name="woundCleaned" value="<?php echo $woundCleaned; ?>" />
+  </div>
+</div>
+<div class="row mb-3">
+  <label for="" class="col-sm-3 col-form-label">Rabies Vaccine</label>
+  <div class="col-sm-6">
+    <input type="text" class="form-control" name="rabiesVaccine" value="<?php echo $rabiesVaccine; ?>" />
+  </div>
+</div>
+<div class="row mb-3">
+  <label for="" class="col-sm-3 col-form-label">Animal Outcome</label>
+  <div class="col-sm-6">
+    <input type="text" class="form-control" name="animalOutcome" value="<?php echo $animalOutcome; ?>" />
+  </div>
+</div>
+<div class="row mb-3">
+  <label for="" class="col-sm-3 col-form-label">Case Class</label>
+  <div class="col-sm-6">
+    <input type="text" class="form-control" name="caseClass" value="<?php echo $caseClass; ?>" />
+  </div>
+</div>
+<div class="row mb-3">
+  <div class="offset-sm-3 col-sm-3 d-grid">
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </div>
+  <div class="col-sm-3 d-grid">
+    <a href="../patientTable.php" class="btn btn-outline-primary" role="button">Cancel</a>
+  </div>
+</div>
