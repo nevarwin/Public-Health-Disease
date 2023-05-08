e17e6f5188cd656d800f6b1da413aa8d4d38e9ae
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-05-08 23:42:52 +0800

    added age and alertmessage

--- components/patientForm-create.php
+++ components/patientForm-create.php
@@ -2,6 +2,8 @@
 // session_start();
 include("connection.php");
 include('barangayScript.php');
+include('alertMessage.php');
+include('ageScript.php');
 // include("function.php");
 
 // $user_data = check_login($con);
@@ -15,8 +17,7 @@ $contact = '';
 $address = '';
 $addressDRU = '';
 
-$errorMessage = '';
-$successMessage = '';
+$alert = '';
 
 // check if the form is submitted using the post method
 // initialize data above into the post
@@ -29,6 +30,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $mName = $_POST['mName'];
     $gender = $_POST['gender'];
     $dob = $_POST['dob'];
+    $age = $_POST['age'];
+    echo $age;
     $municipality = $_POST['municipality'];
     $barangay = $_POST['barangay'];
     $municipalityDRU = $_POST['municipalityDRU'];
@@ -45,17 +48,23 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     do {
         if (empty($fName) or empty($lName) or empty($municipality) or empty($barangay) or empty($municipalityDRU) or empty($barangayDRU) or empty($disease) or empty($contact) or empty($address) or empty($gender)) {
             $errorMessage = "All fields are required";
+            $type = 'warning';
+            $strongContent = 'Holy guacamole!';
+            $alert = generateAlert($type, $strongContent, $errorMessage);
             break;
         }
         // added new data into the db
         $sql = "INSERT INTO patients
-        (`creationDate`, `firstName`, `lastName`, `middleName`, `munCityOfDRU`, `addressOfDRU`,`gender`, `dob`, `municipality`, `barangay`, `address`, `disease`,`brgyOfDRU`, `contact`) 
+        (`creationDate`, `firstName`, `lastName`, `middleName`, `munCityOfDRU`, `addressOfDRU`,`gender`, `dob`, `age`, `municipality`, `barangay`, `address`, `disease`,`brgyOfDRU`, `contact`) 
         VALUES 
-        ('$currentDate', '$fName', '$lName', '$mName' , '$municipalityDRU', '$addressDRU','$gender', '$dob', '$municipality', '$barangay', '$address', '$disease', '$barangayDRU', '$contact')";
+        ('$currentDate', '$fName', '$lName', '$mName' , '$municipalityDRU', '$addressDRU','$gender', '$dob', '$age', '$municipality', '$barangay', '$address', '$disease', '$barangayDRU', '$contact')";
         $result = mysqli_query($con, $sql);
 
         if (!$result) {
             $errorMessage = mysqli_error($con);
+            $type = 'warning';
+            $strongContent = 'Holy guacamole!';
+            $alert = generateAlert($type, $strongContent, $errorMessage);
             break;
         }
 
@@ -66,41 +75,38 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $value = $row['diseaseId'];
         $diseaseValue = strtolower($row['disease']);
 
-
-        echo ($value);
-        echo gettype($value);
-        echo ($diseaseName);
-        echo ($diseaseValue);
-        echo gettype($diseaseValue);
+        echo ($value); //13
+        echo gettype($value); //string
+        echo ($diseaseName); //13
+        echo ($diseaseValue); //rabies
+        echo gettype($diseaseValue); //string
 
         if (strcmp($diseaseName, $value) == 0) {
-            echo "
-            <script> 
-            alert('Patient Successfully Created');
-            window.location= 'http://localhost/admin2gh/patientTable.php';
-            </script>
-            ";
-            $link = "/phpsandbox/publichealth/{$diseaseValue}Form-create.php";
-            echo ($link);
-            header("Location: $link");
+
+            // $link = "/admin2gh/components/{$diseaseValue}Form-create.php";
+            // echo ($link);
+            // header("Location: $link");
             // exit(0);
 
             // check if certain disease form is present *not working
-            if (file_exists($link)) {
-                echo ('Link Exist!');
-                // header("Location: $link");
-                // exit(0);
-            } else {
-                echo ('Link does not exist!');
-            }
+            // if (file_exists($link)) {
+            //     echo ('Link Exist!');
+            //     // header("Location: $link");
+            //     // exit(0);
+            // } else {
+            //     echo ('Link does not exist!');
+            // }
         } else {
             // Handle error
-            echo ('error');
+            $errorMessage = "Link does not exists!";
+            $type = 'warning';
+            $strongContent = 'Holy guacamole!';
+            $alert = generateAlert($type, $strongContent, $errorMessage);
         }
-        $successMessage = "Client added correctly";
-
-        // header("location: /phpsandbox/publichealth/patient.php");
-        // exit;
+        $type = 'success';
+        $strongContent = 'Holy guacamole!';
+        $successMessage = "Patient added correctly";
+        $alert = generateAlert($type, $strongContent, $successMessage);
     } while (false);
 }
 
@@ -110,6 +116,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <div class="row d-flex justify-content-center">
     <div class="col-md-7">
         <h2>New Patient</h2>
+
+        <?php
+        if (!empty($errorMessage)) {
+            echo $alert;
+        }
+        ?>
 
         <form action="" method="post">
             <div class="input-group mb-3">
@@ -144,14 +156,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <div class="row mb-3">
                 <label for="" class='col-sm-3 col-form-label'>Date of Birthday</label>
                 <div class="col-sm-6">
-                    <input type="date" class='form-control' name='dob' id="dob" onchange="calculateAge()" value=''>
+                    <input type="date" class='form-control' name='dob' id="dob" onchange="updateVariable(event)" max="<?php echo date('Y-m-d'); ?>">
                 </div>
             </div>
             <!-- Age minus the DOB -->
             <div class="row mb-3">
                 <label for="" class='col-sm-3 col-form-label'>Age</label>
                 <div class="col-sm-6">
-                    <input type="number" class='form-control' id="age" name='age' disabled>
+                    <input type="text" class='form-control' id="age" name='age'>
                 </div>
             </div>
             <!-- Contact Number -->
@@ -223,7 +235,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <div class="row mb-3">
                 <label class='col-sm-3 col-form-label' for="disease">Disease</label>
                 <div class="col-sm-6">
-                    <select class="form-select" id="disease" name="disease">
+                    <select class="form-select" id="dynamicDisease" name="disease">
                         <option value="">Select Disease</option>
                         <?php
                         // Connect to database and fetch disease
@@ -239,47 +251,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     </select>
                 </div>
             </div>
-            <!-- Outcome Dropdown
-                <div class="row mb-3">
-                    <label class='col-sm-3 col-form-label' for="outcome">Outcome</label>
-                    <div class="col-sm-6">
-                        <select class="form-select" id="outcome" name="outcome">
-                            <option value="">Select Outcome</option>
-                            <?php
-                            // Connect to database and fetch municipalities
-                            include("connection.php");
-                            $result = mysqli_query($con, 'SELECT * FROM outcomes');
-
-                            // Display each municipalities in a dropdown option
-                            while ($row = mysqli_fetch_assoc($result)) {
-                                echo '<option value="' . $row['outcomeId'] . '">' . $row['outcome'] . '</option>';
-                            }
-                            ?>
-                        </select>
-                    </div>
-                </div>
-                DateDied 
-                <div class="row mb-3">
-                    <label for="" class='col-sm-3 col-form-label'>Date Died</label>
-                    <div class="col-sm-6">
-                        <input type="date" class='form-control' name='dateDied' value='<?php echo $dateDied; ?>'>
-                    </div>
-                </div> -->
 
             <?php
             if (!empty($successMessage)) {
-                echo "
-                    <div class='row mb-3'>
-                    <div class='offset-sm-3 col-sm-6'>
-                    <div class='alert alert-success alert-dimissible fade show' role='alert'>
-                    <strong>$successMessage</strong>
-                    <button type = 'button' class = 'btn-close' data-bs-dismissible = 'alert' aria-label = 'close'></button>
-                    </div>
-                    </div>
-                    </div>
-                    ";
+                echo $alert;
             }
             ?>
+            <br>
+            <div id="form-extension"></div>
+            <br>
             <div class="row mb-3">
                 <div class="offset-sm-3 col-sm-3 d-grid">
                     <button type="submit" class='btn btn-primary' name="createPatient">Submit</button>
@@ -291,3 +271,55 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         </form>
     </div>
 </div>
+
+<?php
+include('ageScript.php');
+?>
+
+<!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+    // $(document).ready(function() {
+    //     $('#dropdown').change(function() {
+    //         var selectedValue = $(this).val();
+    //         var value = "<?php echo $value; ?>"
+    //         var diseaseValue = "<?php echo $diseaseValue; ?>"
+    //         var url = diseaseValue + '-form.html';
+
+    //         if (selectedValue === value) {
+    //             $.ajax({
+    //                 url: url,
+    //                 dataType: 'html',
+    //                 success: function(response) {
+    //                     $('#form-extension').html(response);
+    //                 }
+    //             });
+    //         } else {
+    //             $('#form-extension').empty();
+    //         }
+    //     });
+    // });
+
+    document.getElementById('dynamicDisease').addEventListener('change', function() {
+        var selectedValue = this.value;
+        $(document).ready(function() {
+            $('#dropdown').change(function() {
+                var selectedval = $(this).val();
+                // var value = "<?php echo $value; ?>"
+                // var diseaseValue = "<?php echo $diseaseValue; ?>"
+                var url = selectedValue + '-form.html';
+
+                if (selectedValue === '13') {
+                    $.ajax({
+                        url: url,
+                        dataType: 'html',
+                        success: function(response) {
+                            $('#form-extension').html(response);
+                        }
+                    });
+                } else {
+                    $('#form-extension').empty();
+                }
+            });
+        });
+    });
+</script> -->
