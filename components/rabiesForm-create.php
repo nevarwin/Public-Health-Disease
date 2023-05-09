de26559a7a9993f795b414d89239d6777753d77d
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-05-09 12:44:57 +0800

    removed the value

--- components/rabiesForm-create.php
+++ components/rabiesForm-create.php
@@ -1,73 +1,73 @@
 <div class="row mb-3">
   <label for="" class="col-sm-3 col-form-label">Type Of Exposure</label>
   <div class="col-sm-6">
-    <input type="text" class="form-control" name="typeOfExposure" value="<?php echo $typeOfExposure; ?>" />
+    <input type="text" class="form-control" name="typeOfExposure" />
   </div>
 </div>
 <div class="row mb-3">
   <label for="" class="col-sm-3 col-form-label">Category</label>
   <div class="col-sm-6">
-    <input type="text" class="form-control" name="category" value="<?php echo $category; ?>" />
+    <input type="text" class="form-control" name="category" />
   </div>
 </div>
 <div class="row mb-3">
   <label for="" class="col-sm-3 col-form-label">Bite Site</label>
   <div class="col-sm-6">
-    <input type="text" class="form-control" name="biteSite" value="<?php echo $biteSite; ?>" />
+    <input type="text" class="form-control" name="biteSite" />
   </div>
 </div>
 <div class="row mb-3">
   <label for="" class="col-sm-3 col-form-label">Date Bitten</label>
   <div class="col-sm-6">
-    <input type="date" class="form-control" name="dateBitten" value="<?php echo $dateBitten; ?>" />
+    <input type="date" class="form-control" name="dateBitten" />
   </div>
 </div>
 <div class="row mb-3">
   <label for="" class="col-sm-3 col-form-label">Type Of Animal</label>
   <div class="col-sm-6">
-    <input type="text" class="form-control" name="typeOfAnimal" value="<?php echo $typeOfAnimal; ?>" />
+    <input type="text" class="form-control" name="typeOfAnimal" />
   </div>
 </div>
 <div class="row mb-3">
   <label for="" class="col-sm-3 col-form-label">Animal Status</label>
   <div class="col-sm-6">
-    <input type="text" class="form-control" name="animalStatus" value="<?php echo $animalStatus; ?>" />
+    <input type="text" class="form-control" name="animalStatus" />
   </div>
 </div>
 <div class="row mb-3">
   <label for="" class="col-sm-3 col-form-label">Date Vacc Started</label>
   <div class="col-sm-6">
-    <input type="date" class="form-control" name="dateVaccStarted" value="<?php echo $dateVaccStarted; ?>" />
+    <input type="date" class="form-control" name="dateVaccStarted" />
   </div>
 </div>
 <div class="row mb-3">
   <label for="" class="col-sm-3 col-form-label">Animal Vaccination</label>
   <div class="col-sm-6">
-    <input type="text" class="form-control" name="animalVacc" value="<?php echo $animalVacc; ?>" />
+    <input type="text" class="form-control" name="animalVacc" />
   </div>
 </div>
 <div class="row mb-3">
   <label for="" class="col-sm-3 col-form-label">Wound Cleaned</label>
   <div class="col-sm-6">
-    <input type="text" class="form-control" name="woundCleaned" value="<?php echo $woundCleaned; ?>" />
+    <input type="text" class="form-control" name="woundCleaned" />
   </div>
 </div>
 <div class="row mb-3">
   <label for="" class="col-sm-3 col-form-label">Rabies Vaccine</label>
   <div class="col-sm-6">
-    <input type="text" class="form-control" name="rabiesVaccine" value="<?php echo $rabiesVaccine; ?>" />
+    <input type="text" class="form-control" name="rabiesVaccine" />
   </div>
 </div>
 <div class="row mb-3">
   <label for="" class="col-sm-3 col-form-label">Animal Outcome</label>
   <div class="col-sm-6">
-    <input type="text" class="form-control" name="animalOutcome" value="<?php echo $animalOutcome; ?>" />
+    <input type="text" class="form-control" name="animalOutcome" />
   </div>
 </div>
 <div class="row mb-3">
   <label for="" class="col-sm-3 col-form-label">Case Class</label>
   <div class="col-sm-6">
-    <input type="text" class="form-control" name="caseClass" value="<?php echo $caseClass; ?>" />
+    <input type="text" class="form-control" name="caseClass" />
   </div>
 </div>
 <div class="row mb-3">
