69f70f429e58f6e26afd4e7585b91d5ba6a73fab
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-05-09 23:30:20 +0800

    edited

--- components/topbar.php
+++ components/topbar.php
@@ -1,6 +1,5 @@
 <!-- Topbar -->
 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
     <!-- Sidebar Toggle (Topbar) -->
     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
         <i class="fa fa-bars"></i>
@@ -24,13 +23,11 @@
                 <i class="fas fa-search fa-fw"></i>
             </a>
         </li>
-
         <div class="topbar-divider d-none d-sm-block"></div>
-
         <!-- Nav Item - User Information -->
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $user_data['name']; ?></span>
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $user_data['name']; ?></span>
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">(Super Admin)</span>
                 <img class="img-profile rounded-circle" src="img/undraw_profile.svg" />
             </a>
