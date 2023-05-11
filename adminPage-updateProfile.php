c12867bdace0283dd85522f0cfa9b37e2f6945b2
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-05-11 11:18:53 +0800

    changed the h1

--- adminPage-updateProfile.php
+++ adminPage-updateProfile.php
@@ -22,7 +22,7 @@ include('./components/head.php');
                 <div class="container-fluid">
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">Update Admin</h1>
+                        <h1 class="h3 mb-0 text-gray-800">Update Profile</h1>
 
                     </div>
                     <?php
