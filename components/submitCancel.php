a71dc8f3a4827af38d522b559aec865478d61337
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-05-11 23:47:06 +0800

    separated submit and cancel buttons

---
+++ components/submitCancel.php
@@ -0,0 +1,8 @@
+<div class="row mb-3">
+    <div class="offset-sm-3 col-sm-3 d-grid">
+        <button type="submit" class='btn btn-primary'>Submit</button>
+    </div>
+    <div class="col-sm-3 d-grid">
+        <a href="http://localhost/admin2gh/patientPage-view.php?patientId=<?php echo $patientId; ?>" class="btn btn-outline-primary" role="button">Cancel</a>
+    </div>
+</div>
