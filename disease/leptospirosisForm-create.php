7ff6240a722a31bff5e99938e9e6ad1177e5d21a
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-05-13 01:19:19 +0800

    created leptospirosis crud

---
+++ disease/leptospirosisForm-create.php
@@ -0,0 +1,156 @@
+<?php
+include('./components/alertMessage.php');
+include("./components/connection.php");
+
+$user_data = check_login($con);
+
+$patientId = '';
+
+$message = '';
+$type = '';
+$strongContent = '';
+
+// if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+//GET Method: show the data of the client
+if (!isset($_GET["patientId"])) {
+    echo "User ID is not set.";
+    // header('location: http://localhost/admin2gh/patientTable.php');
+    // exit;
+}
+$patientId = $_GET['patientId'];
+
+if (empty($patientId)) {
+    echo 'patiend Id emtpy';
+}
+echo $patientId;
+
+// check if the form is submitted using the post method
+// initialize data above into the post
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Retrieve the form data
+    $labResult = $_POST['labResult'];
+    $serovar = $_POST['serovar'];
+    $caseClass = $_POST['caseClass'];
+    $outcome = $_POST['outcome'];
+    $dateDied = $_POST['dateDied'];
+    $dateAdmitted = $_POST['dateAdmitted'];
+    $morbidityMonth = $_POST['morbidityMonth'];
+    $morbidityWeek = $_POST['morbidityWeek'];
+
+
+    // check if the data is empty
+    do {
+        if (empty($dateAdmitted)) {
+            $errorMessage = "All fields are required!";
+            echo "<script>alert('All fields are required!');</script>";
+            break;
+        }
+        // Proceed with form submission
+        // Insert the data into the leptospirosisinfotbl table
+        $query = "INSERT INTO leptospirosisinfotbl (
+                    patientId,
+                    labResult,
+                    serovar,
+                    caseClass,
+                    outcome,
+                    dateDied,
+                    dateAdmitted,
+                    morbidityMonth,
+                    morbidityWeek
+                )
+                VALUES (
+                    '$patientId',
+                    '$labResult',
+                    '$serovar',
+                    '$caseClass',
+                    '$outcome',
+                    '$dateDied',
+                    '$dateAdmitted',
+                    '$morbidityMonth',
+                    '$morbidityWeek'
+                )";
+
+
+        $result = mysqli_query($con, $query);
+
+        if ($result) {
+            $message = "Leptospirosis info successfully added!";
+            $type = 'success';
+            $strongContent = 'Holy guacamole!';
+            $alert = generateAlert($type, $strongContent, $message);
+
+            echo "<script>
+                alert('Leptospirosis form submitted successfully!');
+                window.location = 'http://localhost/admin2gh/patientTable.php';
+            </script>";
+            exit;
+        } else {
+            $message = "Error submitting form!" . mysqli_error($con);
+            $type = 'warning';
+            $strongContent = 'Holy guacamole!';
+            $alert = generateAlert($type, $strongContent, $message);
+        }
+    } while (false);
+}
+
+?>
+
+<form method="POST">
+    <?php
+    if (!empty($alert)) {
+        echo $alert;
+    }
+    ?>
+    <div class="row mb-3">
+        <label for="" class="col-sm-3 col-form-label">Date Admitted</label>
+        <div class="col-sm-6">
+            <input type="date" class="form-control" name="dateAdmitted" max="<?php echo date('Y-m-d'); ?>" />
+        </div>
+    </div>
+    <div class="row mb-3">
+        <label class="col-sm-3 form-label">Lab Result</label>
+        <div class="col-sm-6">
+            <input type="text" class="form-control" id="labResult" name="labResult">
+        </div>
+    </div>
+    <div class="row mb-3">
+        <label class="col-sm-3 form-label">Serovar</label>
+        <div class="col-sm-6">
+            <input type="text" class="form-control" id="serovar" name="serovar">
+        </div>
+    </div>
+    <div class="row mb-3">
+        <label class="col-sm-3 form-label">Case Class</label>
+        <div class="col-sm-6">
+            <input type="text" class="form-control" id="caseClass" name="caseClass">
+        </div>
+    </div>
+    <div class="row mb-3">
+        <label class="col-sm-3 form-label">MorbidityWeek</label>
+        <div class="col-sm-6">
+            <input type="text" class="form-control" name="morbidityWeek">
+        </div>
+    </div>
+    <div class="row mb-3">
+        <label class="col-sm-3 form-label">morbidityMonth</label>
+        <div class="col-sm-6">
+            <input type="text" class="form-control" name="morbidityMonth">
+        </div>
+    </div>
+    <div class="row mb-3">
+        <label class="col-sm-3 form-label">Outcome</label>
+        <div class="col-sm-6">
+            <input type="text" class="form-control" id="outcome" name="outcome">
+        </div>
+    </div>
+    <div class="row mb-3">
+        <label class="col-sm-3 form-label">Date Died</label>
+        <div class="col-sm-6">
+
+            <input type="datetime-local" class="col-sm-3 form-control" id="dateDied" name="dateDied">
+        </div>
+    </div>
+    <div class="col-sm-3 mb-3">
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+</form>
---
+++ disease/leptospirosisForm-update.php
@@ -0,0 +1,157 @@
+<?php
+include('./components/alertMessage.php');
+include("./components/connection.php");
+
+$user_data = check_login($con);
+
+$patientId = '';
+
+$message = '';
+$type = '';
+$strongContent = '';
+
+// if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+//GET Method: show the data of the client
+if (!isset($_GET["patientId"])) {
+    echo "User ID is not set.";
+    // header('location: http://localhost/admin2gh/patientTable.php');
+    // exit;
+}
+$patientId = $_GET['patientId'];
+// read row 
+$sql = "SELECT * FROM leptospirosisinfotbl WHERE patientId = $patientId";
+// execute the sql query
+$result = mysqli_query($con, $sql);
+$row = $result->fetch_assoc();
+
+if (!$row) {
+    echo "error in row";
+    header('location: http://localhost/admin2gh/patientTable.php');
+    exit;
+}
+$labResult = $row['labResult'];
+$serovar = $row['serovar'];
+$caseClass = $row['caseClass'];
+$outcome = $row['outcome'];
+$dateDied = $row['dateDied'];
+$dateAdmitted = $row['dateAdmitted'];
+$morbidityMonth = $row['morbidityMonth'];
+$morbidityWeek = $row['morbidityWeek'];
+
+// check if the form is submitted using the post method
+// initialize data above into the post
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Retrieve the form data
+    $labResult = $_POST['labResult'];
+    $serovar = $_POST['serovar'];
+    $caseClass = $_POST['caseClass'];
+    $outcome = $_POST['outcome'];
+    $dateDied = $_POST['dateDied'];
+    $dateAdmitted = $_POST['dateAdmitted'];
+    $morbidityMonth = $_POST['morbidityMonth'];
+    $morbidityWeek = $_POST['morbidityWeek'];
+    // check if the data is empty
+    do {
+        if (empty($dateAdmitted)) {
+            $errorMessage = "All fields are required!";
+            echo "<script>alert('All fields are required!');</script>";
+            break;
+        }
+        // Proceed with form submission
+        // Insert the data into the leptospirosisinfotbl table
+        $query = "UPDATE leptospirosisinfotbl
+                SET
+                    labResult = '$labResult',
+                    serovar = '$serovar',
+                    caseClass = '$caseClass',
+                    outcome = '$outcome',
+                    dateDied = '$dateDied',
+                    dateAdmitted = '$dateAdmitted',
+                    morbidityMonth = '$morbidityMonth',
+                    morbidityWeek = '$morbidityWeek'
+                WHERE patientId = '$patientId'";
+
+        $result = mysqli_query($con, $query);
+
+        if ($result) {
+            $message = "Leptospirosis info successfully updated!";
+            $type = 'success';
+            $strongContent = 'Holy guacamole!';
+            $alert
+                = generateAlert($type, $strongContent, $message);
+
+            echo "<script>
+                alert('Leptospirosis info successfully updated!');
+                window.location = 'http://localhost/admin2gh/patientPage-view.php?patientId=$patientId';
+            </script>";
+            exit;
+        } else {
+            $message = "Error submitting form! " . mysqli_error($con);
+            $type = 'warning';
+            $strongContent = 'Holy guacamole!';
+            $alert = generateAlert($type, $strongContent, $message);
+        }
+    } while (false);
+}
+
+?>
+
+<form method="POST">
+    <?php
+    if (!empty($alert)) {
+        echo $alert;
+    }
+    ?>
+
+    <div class="row mb-3">
+        <label for="" class="col-sm-3 form-label">Date Admitted</label>
+        <div class="col-sm-6">
+            <input type="date" class="form-control" name="dateAdmitted" max="<?php echo date('Y-m-d'); ?>" value='<?php echo $dateAdmitted; ?>' />
+        </div>
+    </div>
+    <div class="row mb-3">
+        <label class="col-sm-3 form-label">Lab Result</label>
+        <div class="col-sm-6">
+            <input type="text" class="form-control" id="labResult" name="labResult" value='<?php echo $labResult; ?>'>
+        </div>
+    </div>
+    <div class="row mb-3">
+        <label class="col-sm-3 form-label">Serovar</label>
+        <div class="col-sm-6">
+            <input type="text" class="form-control" id="serovar" name="serovar" value='<?php echo $serovar; ?>'>
+        </div>
+    </div>
+    <div class="row mb-3">
+        <label class="col-sm-3 form-label">Case Classification</label>
+        <div class="col-sm-6">
+            <input type="text" class="form-control" id="caseClass" name="caseClass" value='<?php echo $caseClass; ?>'>
+        </div>
+    </div>
+    <div class="row mb-3">
+        <label class="col-sm-3 form-label">morbidityMonth</label>
+        <div class="col-sm-6">
+            <input type="text" class="form-control" name="morbidityMonth" value='<?php echo $morbidityMonth; ?>' />
+        </div>
+    </div>
+    <div class="row mb-3">
+        <label class="col-sm-3 form-label">MorbidityWeek</label>
+        <div class="col-sm-6">
+            <input type="text" class="form-control" name="morbidityWeek" value='<?php echo $morbidityWeek; ?>' />
+        </div>
+    </div>
+    <div class="row mb-3">
+        <label class="col-sm-3 form-label">Outcome</label>
+        <div class="col-sm-6">
+            <input type="text" class="form-control" id="outcome" name="outcome" value='<?php echo $outcome; ?>'>
+        </div>
+    </div>
+    <div class="row mb-3">
+        <label class="col-sm-3 form-label">Date Died</label>
+        <div class="col-sm-6">
+            <input type="datetime-local" class="form-control" id="dateDied" name="dateDied" value='<?php echo $dateDied; ?>'>
+        </div>
+    </div>
+    <?php
+    include('./components/submitCancel.php');
+    ?>
+</form>
