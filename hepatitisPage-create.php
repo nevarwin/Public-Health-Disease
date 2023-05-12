27157a8a9c5e1b5c18bc2c4db80803d886fc0235
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-05-12 12:41:24 +0800

    created hepatitis page

---
+++ hepatitisPage-create.php
@@ -0,0 +1,49 @@
+<?php
+include('./components/head.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<body id="page-top">
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+        <?php
+        include('./components/sidebar.php');
+        ?>
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+            <!-- Main Content -->
+            <div id="content">
+                <?php
+                include('./components/topbar.php');
+                ?>
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+                    <!-- Page Heading -->
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 class="h3 mb-0 text-gray-800">Create Hepatitis Form</h1>
+                    </div>
+                    <?php
+                    include('./disease/hepatitisForm-create.php');
+                    ?>
+                </div>
+                <!-- /.container-fluid -->
+            </div>
+            <!-- End of Main Content -->
+            <?php
+            include('./components/footer.php');
+            ?>
+        </div>
+        <!-- End of Content Wrapper -->
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+    <?php
+    include('./components/logoutmodal.php');
+    include('./components/script.php');
+    ?>
