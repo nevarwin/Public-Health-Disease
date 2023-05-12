df9fe9221e713025de2f06a0db77d351a6c96648
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-05-12 12:53:32 +0800

    created meningitis crud

---
+++ disease/meningitisForm-view.php
@@ -0,0 +1,105 @@
+<?php
+include('./components/alertMessage.php');
+include("./components/connection.php");
+
+$user_data = check_login($con);
+
+$patientId = '';
+
+$message = '';
+$type = '';
+$strongContent = '';
+
+// if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+//GET Method: show the data of the client
+if (!isset($_GET["patientId"])) {
+    echo "User ID is not set.";
+    // header('location: http://localhost/admin2gh/patientTable.php');
+    // exit;
+}
+$patientId = $_GET['patientId'];
+// read row 
+$sql = "SELECT * FROM meningitisinfotbl WHERE patientId = $patientId";
+// execute the sql query
+$result = mysqli_query($con, $sql);
+$row = $result->fetch_assoc();
+
+if (!$row) {
+    echo "error in row";
+    header('location: http://localhost/admin2gh/patientTable.php');
+    exit;
+}
+$type = $row['type'];
+$labResult = $row['labResult'];
+$organism = $row['organism'];
+$caseClass = $row['caseClass'];
+$outcome = $row['outcome'];
+$dateDied = $row['dateDied'];
+$dateAdmitted = $row['dateAdmitted'];
+$morbidityMonth = $row['morbidityMonth'];
+$morbidityWeek = $row['morbidityWeek'];
+
+?>
+
+<form method="POST">
+    <?php
+    if (!empty($alert)) {
+        echo $alert;
+    }
+    ?>
+
+    <div class="row mb-3">
+        <label for="" class="col-sm-3 form-label">Date Admitted</label>
+        <div class="col-sm-6">
+            <p> <?php echo $dateAdmitted; ?> </p>
+        </div>
+    </div>
+    <div class="row mb-3">
+        <label class="col-sm-3 form-label">Type</label>
+        <div class="col-sm-6">
+            <p> <?php echo $type; ?> </p>
+        </div>
+    </div>
+    <div class="row mb-3">
+        <label class="col-sm-3 form-label">Lab Result</label>
+        <div class="col-sm-6">
+            <p> <?php echo $labResult; ?></p>
+        </div>
+    </div>
+    <div class="row mb-3">
+        <label class="col-sm-3 form-label">Organism</label>
+        <div class="col-sm-6">
+            <p> <?php echo $organism; ?> </p>
+        </div>
+    </div>
+    <div class="row mb-3">
+        <label class="col-sm-3 form-label">Case Classification</label>
+        <div class="col-sm-6">
+            <p> <?php echo $caseClass; ?></p>
+        </div>
+    </div>
+    <div class="row mb-3">
+        <label class="col-sm-3 form-label">morbidityMonth</label>
+        <div class="col-sm-6">
+            <p> <?php echo $morbidityMonth; ?></p>
+        </div>
+    </div>
+    <div class="row mb-3">
+        <label class="col-sm-3 form-label">MorbidityWeek</label>
+        <div class="col-sm-6">
+            <p> <?php echo $morbidityWeek; ?></p>
+        </div>
+    </div>
+    <div class="row mb-3">
+        <label class="col-sm-3 form-label">Outcome</label>
+        <div class="col-sm-6">
+            <p> <?php echo $outcome; ?></p>
+        </div>
+    </div>
+    <div class="row mb-3">
+        <label class="col-sm-3 form-label">Date Died</label>
+        <div class="col-sm-6">
+            <p> <?php echo $dateDied; ?> </p>
+        </div>
+    </div>
+</form>
