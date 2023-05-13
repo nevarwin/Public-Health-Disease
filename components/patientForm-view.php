170a2c408302381bdd0405323fc6e325d6b11376
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-05-13 20:12:24 +0800

    adjust the size

--- components/patientForm-view.php
+++ components/patientForm-view.php
@@ -207,7 +207,7 @@ $barangayDRU = $barangayDRURow['barangay'];
             </div>
         </div>
         <!-- Second Column -->
-        <div id="findings" class="col-xl-6 col-lg-4">
+        <div id="findings" class="col-xl-6 col-lg-7">
             <div class="card shadow mb-4">
                 <!-- Card Header - Dropdown -->
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
