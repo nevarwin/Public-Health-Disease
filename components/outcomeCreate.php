0a302f19191ce38b7c5ba5e05309f470d81b2225
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-05-15 23:27:49 +0800

    renamed

---
+++ components/outcomeCreate.php
@@ -0,0 +1,37 @@
+<div class="row mb-3">
+    <label for="outcome" class="col-sm-3 form-label">Outcome</label>
+    <div class="col-sm-6">
+        <select class="custom-select" id="outcome" name="outcome" onchange="toggleDateDied()">
+            <?php
+            $outcomeOptions = array('alive', 'dead', 'other'); // Add more options if needed
+            foreach ($outcomeOptions as $option) {
+                $selected = ($outcome === $option) ? 'selected' : '';
+                echo '<option value="' . $option . '" ' . $selected . '>' . ucfirst($option) . '</option>';
+            }
+            ?>
+        </select>
+    </div>
+</div>
+<div class="row mb-3">
+    <label for="dateDied" class="col-sm-3 form-label">Date Died</label>
+    <div class="col-sm-6">
+        <input type="datetime-local" class="form-control" id="dateDied" name="dateDied">
+    </div>
+</div>
+
+<script>
+    function toggleDateDied() {
+        const outcome = document.getElementById('outcome').value;
+        const dateDied = document.getElementById('dateDied');
+
+        if (outcome === 'dead') {
+            dateDied.disabled = false;
+        } else {
+            dateDied.disabled = true;
+        }
+    }
+
+    // Call the function on page load to set the initial state
+    // toggleDateDied();
+    window.onload = toggleDateDied;
+</script>
