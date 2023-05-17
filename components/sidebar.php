3931c8127418c93704f597649727faeec2af4c26
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-05-17 11:31:34 +0800

    added logo but not working

--- components/sidebar.php
+++ components/sidebar.php
@@ -2,14 +2,14 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="homepage.php">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
         <div class="sidebar-brand-icon rotate-n-15">
             <!-- <i class="fas fa-laugh-wink"></i> -->
         </div>
-        <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-laugh-wink"></i>
+        <div class="sidebar-brand-icon">
+            <img src="C:\xampp\htdocs\admin2gh\assets\img\caviteLogo.png" alt="Logo">
         </div>
-        <div class="sidebar-brand-text mx-1">Public Health Disease Geomapping</div>
+        <div class="sidebar-brand-text ">Public Health Disease Geomapping</div>
     </a>
 
     <!-- Divider -->
