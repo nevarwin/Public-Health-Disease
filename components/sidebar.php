2cd123cddf55c27f494303c675ac757dcad562c0
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-05-18 12:19:06 +0800

    added tab icon

--- components/sidebar.php
+++ components/sidebar.php
@@ -3,11 +3,8 @@
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
-        <div class="sidebar-brand-icon rotate-n-15">
-            <!-- <i class="fas fa-laugh-wink"></i> -->
-        </div>
         <div class="sidebar-brand-icon">
-            <img src="C:\xampp\htdocs\admin2gh\assets\img\caviteLogo.png" alt="Logo">
+            <img src="./assets/img/caviteLogo.png" alt="Logo" style="height:50px; width:50px;">
         </div>
         <div class="sidebar-brand-text ">Public Health Disease Geomapping</div>
     </a>
