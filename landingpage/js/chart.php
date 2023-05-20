059e5fb7ca2e51ebbe63e56641e0f975d884f799
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-05-21 00:26:38 +0800

    edited for real data

--- landingpage/js/chart.php
+++ landingpage/js/chart.php
@@ -5,25 +5,23 @@
     legend: 'Hellow',
     type: "bar",
     data: {
-      labels: ["2018", "2019", "2020", "2021", "2022", "2023"],
+      labels: ["2018", "2019", "2020", "2021", "2022"],
       datasets: [{
-        label: "Number of Dengue Cases",
-        data: [280, 413, 312, 415, 615, 578, 894],
+        label: "Number of Influenza Cases",
+        data: [2358, 3877, 2850, 3504, 5885],
         backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
           'rgba(255, 159, 64, 0.2)',
           'rgba(255, 205, 86, 0.2)',
           'rgba(75, 192, 192, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(153, 102, 255, 0.2)'
+          'rgba(54, 162, 235, 0.2)'
         ],
         borderColor: [
           'rgb(255, 99, 132)',
           'rgb(255, 159, 64)',
           'rgb(255, 205, 86)',
           'rgb(75, 192, 192)',
-          'rgb(54, 162, 235)',
-          'rgb(153, 102, 255)'
+          'rgb(54, 162, 235)'
         ],
         borderWidth: 1,
       }, ],
@@ -32,7 +30,7 @@
       plugins: {
         title: {
           display: true,
-          text: 'Dungue Cases',
+          text: 'Influenza Cases in past 5 years',
           font: {
             size: 18
           }
@@ -54,23 +52,26 @@
 
   const pieChart = document.getElementById("pieChart");
 
+  const colors = [];
+  for (let i = 0; i < 23; i++) {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    const alpha = (Math.floor(Math.random() * 9) + 1) / 10; // Random alpha value between 0.1 and 0.9
+    const color = `rgba(${red}, ${green}, ${blue}, ${alpha})`;
+    colors.push(color);
+  }
+
+
   new Chart(pieChart, {
     type: "pie",
     data: {
-      labels: ["Alive", "Dead", "Recovering"],
+      labels: ["Alfonso", "Amadeo", "Bacoor", "Carmona", "Cavite City", "Dasmariñas", "Gen. Emilio Aguinaldo", "Gen. Mariano Alvarez", "General Trias", "Imus", "Indang", "Kawit", "Magallanes", "Maragondon", "Mendez", "Naic", "Noveleta", "Rosario", "Silang", "Tagaytay City", "Tanza", "Ternate", "Trece Martires City"],
       datasets: [{
-        label: "Mortality Rate of Influenza Year 2021",
-        data: [205, 13, 89],
-        backgroundColor: [
-          'rgba(75, 192, 192, 0.5)',
-          'rgba(255, 99, 132, 0.5)',
-          'rgba(255, 159, 64, 0.5)',
-        ],
-        borderColor: [
-          'rgb(75, 192, 192)',
-          'rgb(255, 99, 132)',
-          'rgb(255, 159, 64)',
-        ],
+        label: "Cases",
+        data: [66, 50, 1362, 33, 9, 133, 16, 109, 207, 2809, 6, 108, 2, 72, 81, 14, 10, 17, 190, 301, 59, 215, 16],
+        backgroundColor: colors,
+        borderColor: colors,
         borderWidth: 1,
       }, ],
     },
@@ -78,7 +79,7 @@
       plugins: {
         title: {
           display: true,
-          text: 'Influenza Year 2021',
+          text: 'Influenza Cases Per Municipality Year 2022',
           font: {
             size: 18
           }
