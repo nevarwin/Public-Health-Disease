45f19d016ba06127073816ae9c609bab9d5e74ac
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-05-24 00:36:50 +0800

    added generate report php

--- patientTable.php
+++ patientTable.php
@@ -22,7 +22,7 @@ include('./components/head.php');
                 <div class="container-fluid">
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 class="h3 mb-0 text-gray-800">Patients</h1>
-                        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+                        <a href="generate_report.php" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
                     </div>
                     <?php
                     // include('./components/pageheading.php');
