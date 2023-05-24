a6ed25b33bc4b4409a05e717ff10d5ffa406e297
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-05-25 00:36:03 +0800

    adjusted the user name and position

--- components/topbar.php
+++ components/topbar.php
@@ -27,18 +27,12 @@ $positionValue = $positionRow['position'];
     </form> -->
     <!-- Topbar Navbar -->
     <ul class="navbar-nav ml-auto">
-        <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-        <li class="nav-item dropdown no-arrow d-sm-none">
-            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-search fa-fw"></i>
-            </a>
-        </li>
-        <div class="topbar-divider d-none d-sm-block"></div>
         <!-- Nav Item - User Information -->
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $user_data['name']; ?></span>
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $positionValue ?></span>
+                <span class="mr-2 d-lg-inline d-md-inline d-sm-inline d-xs-inline text-gray-600 small"><?= $positionValue ?></span>
+                <div class="topbar-divider d-none d-sm-block"></div>
+                <span class="mr-2 d-lg-inline d-md-inline d-sm-inline d-xs-inline text-gray-600 small"><?= $user_data['name']; ?></span>
                 <img class="img-profile rounded-circle" src="img/undraw_profile.svg" />
             </a>
             <!-- Dropdown - User Information -->
