c5646fbd6e91da6a80f1476bb3fed021e866a9d2
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-05-27 15:44:52 +0800

    edited and added dynamic charts

--- charts.php
+++ charts.php
@@ -21,74 +21,42 @@ include('./components/head.php');
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
                     <div class="row">
-                        <!-- Example single danger button -->
-                        <div class="btn-group col-xl-1 col-lg-5 mb-2">
-                            <div class="dropdown">
-                                <button type="button" class="btn btn-primary dropdown-toggle mr-2" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Disease
-                                </button>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                    <li><a class="dropdown-item" href="#">Separated link</a></li>
-                                </ul>
-                            </div>
-                            <div class="dropdown">
-                                <button type="button" class="btn btn-secondary dropdown-toggle mr-4" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Year
-                                </button>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
+                        <!-- <div class="dropdown mx-2">
+            <select class="custom-select" name="year">
+                <?php
+                foreach ($year as $key => $value) {
+                    $selected = ($key == $year) ? 'selected' : '';
+                    echo '<option value="' . $key . '" ' . $selected . '>' . $value . '</option>';
+                }
+                ?>
+                </select>
+            </div> -->
                         <div class="col-md-6">
-                            <div class="card shadow">
-                                <!-- Card Header -->
-                                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                    <h6 class="m-0 font-weight-bold text-primary">Yearly Disease Cases</h6>
-                                </div>
-                                <div class="card-body">
-                                    <canvas id="myChart"></canvas>
-                                </div>
-                            </div>
+                            <?php
+                            include('./components/linechart.php')
+                            ?>
                         </div>
                         <div class="col-md-4">
-                            <div class="card shadow">
-                                <!-- Card Header -->
-                                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                    <h6 class="m-0 font-weight-bold text-primary">Pie Chart</h6>
-                                </div>
-                                <div class="card-body">
-                                    <canvas id="pieChart"></canvas>
-                                </div>
-                            </div>
+                            <?php
+                            include('./components/piechart.php')
+                            ?>
                         </div>
                     </div>
+                    <!-- /.container-fluid -->
                 </div>
-                <!-- /.container-fluid -->
+                <!-- End of Main Content -->
+                <?php
+                include('./components/footer.php');
+                ?>
             </div>
-            <!-- End of Main Content -->
-            <?php
-            include('./components/footer.php');
-            ?>
+            <!-- End of Content Wrapper -->
         </div>
-        <!-- End of Content Wrapper -->
-    </div>
-    <!-- End of Page Wrapper -->
+        <!-- End of Page Wrapper -->
 
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
-    </a>
-    <?php
-    include('./components/logoutmodal.php');
-    // include('./components/script.php');
-    include('./components/linechart.php');
-    include('./components/piechart.php');
-    ?>
+        <!-- Scroll to Top Button-->
+        <a class="scroll-to-top rounded" href="#page-top">
+            <i class="fas fa-angle-up"></i>
+        </a>
+        <?php
+        include('./components/logoutmodal.php');
+        ?>
