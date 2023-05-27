bcdb31e8d42fda5578a9c9e983e998102978d2c7
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-05-28 00:01:35 +0800

    created a separate pieChartPage

---
+++ pieChartPage.php
@@ -0,0 +1,78 @@
+<?php
+include('./components/head.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<body id="page-top">
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+        <?php
+        include('./components/sidebar.php');
+        ?>
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+            <!-- Main Content -->
+            <div id="content">
+                <?php
+                include('./components/topbar.php');
+                ?>
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+                    <div class="row">
+                        <!-- <div class="col-md-6">
+                            <?php
+                            include('./components/linechart.php')
+                            ?>
+                        </div> -->
+                        <div class="col-md-6 col-lg-8 py-2">
+                            <?php
+                            include('./components/piechart.php')
+                            ?>
+                        </div>
+                    </div>
+                    <!-- /.container-fluid -->
+                </div>
+                <!-- End of Main Content -->
+                <?php
+                include('./components/footer.php');
+                ?>
+            </div>
+            <!-- End of Content Wrapper -->
+        </div>
+        <!-- End of Page Wrapper -->
+
+        <!-- Scroll to Top Button-->
+        <a class="scroll-to-top rounded" href="#page-top">
+            <i class="fas fa-angle-up"></i>
+        </a>
+        <?php
+        include('./components/logoutmodal.php');
+        ?>
+
+        <!-- <script>
+            // Get references to the form elements
+            var form1 = document.getElementById('form1');
+            var form2 = document.getElementById('form2');
+
+            // Attach event listener for Form 1 submission
+            form1.addEventListener('submit', function(event) {
+                // event.preventDefault(); // Prevent form submission from reloading the page
+
+                // Handle Form 1 submission logic
+                // ...
+
+                // Optionally, you can reset the form after submission
+            });
+
+            // Attach event listener for Form 2 submission
+            form2.addEventListener('submit', function(event) {
+                event.preventDefault(); // Prevent form submission from reloading the page
+
+                // Handle Form 2 submission logic
+                // ...
+
+                // Optionally, you can reset the form after submission
+            });
+        </script> -->
