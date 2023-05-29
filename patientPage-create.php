ec332c75e7cb83f133384389a44127ff76e22232
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-05-29 23:34:40 +0800

    added a medical logo bg

--- patientPage-create.php
+++ patientPage-create.php
@@ -5,6 +5,38 @@ include('./components/head.php');
 <!DOCTYPE html>
 <html lang="en">
 
+<style>
+    body {
+        overflow: hidden;
+    }
+
+    .background-image {
+        position: fixed;
+        bottom: 10%;
+        right: -5%;
+        z-index: -1;
+        height: 50%;
+        width: 50%;
+        background-image: url('./assets/img/medlogo.png');
+        background-size: 40%;
+        background-position: bottom right;
+        background-repeat: no-repeat;
+        opacity: 0.3;
+    }
+
+    #wrapper {
+        position: relative;
+        z-index: 2;
+    }
+
+    #content-wrapper {
+        position: relative;
+        z-index: 1;
+        overflow-y: auto;
+        max-height: 100vh;
+    }
+</style>
+
 <body id="page-top">
     <!-- Page Wrapper -->
     <div id="wrapper">
@@ -13,6 +45,7 @@ include('./components/head.php');
         ?>
         <!-- Content Wrapper -->
         <div id="content-wrapper" class="d-flex flex-column">
+            <div class="background-image"></div>
             <!-- Main Content -->
             <div id="content">
                 <?php
@@ -25,7 +58,6 @@ include('./components/head.php');
                     include('./components/patientForm-create.php');
                     ?>
                 </div>
-                <!-- /.container-fluid -->
             </div>
             <!-- End of Main Content -->
             <?php
