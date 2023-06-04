b952ea422b5498ef69513b54e754c7fbded25ee0
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-06-05 00:28:42 +0800

    added code for authorize users only

--- components/headcards.php
+++ components/headcards.php
@@ -36,6 +36,40 @@
     }
     ?>
 
+    <!-- <?php
+            // Replace with your database connection code
+            include('./components/connection.php');
+
+            $query = "SELECT COUNT(*) AS count
+            FROM clients
+            WHERE positionId = 3";
+            $result = mysqli_query($con, $query);
+
+            // Display the count of authorized users
+            while ($row = mysqli_fetch_assoc($result)) {
+                $count = $row['count'];
+            ?>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Authorized Users</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $count ?></div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-users fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    <?php
+            }
+    ?> -->
+
     <?php
     // Replace with your database connection code
     include('./components/connection.php');
