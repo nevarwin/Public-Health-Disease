5b9632076f08a5c5a81c0d165a4c74797667c804
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-06-04 23:57:20 +0800

    created separate heatmap js

---
+++ landingpage/js/map.js
@@ -0,0 +1,23 @@
+let map;
+let heatmap;
+// var locationData;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: {
+      lat: 14.278023306102497,
+      lng: 120.88505851514495,
+    },
+    zoom: 11,
+    mapTypeId: "satellite",
+  });
+
+  heatmap = new google.maps.visualization.HeatmapLayer({
+    data: locationData.map(
+      (item) => new google.maps.LatLng(item.lat, item.lng)
+    ),
+    map: map,
+  });
+}
+
+initMap();
