238dfb4037e6627f5872acda4b70e8b07b40ad40
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-06-06 00:27:40 +0800

    added homepage to the logo

--- components/sidebar.php
+++ components/sidebar.php
@@ -2,7 +2,7 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="homepage.php">
         <div class="sidebar-brand-icon">
             <img src="./assets/img/caviteLogo.png" alt="Logo" style="height:50px; width:50px;">
         </div>
