bdaf73a35dbecb3ceae772be6ce3cf37c543c1a7
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-06-06 00:27:48 +0800

    updated the ui

--- components/submitCancel.php
+++ components/submitCancel.php
@@ -1,8 +1,8 @@
-<div class="row mb-3">
-    <div class="offset-sm-3 col-sm-3 d-grid">
+<div class="row justify-content-around">
+    <div class="row justify-content-center">
         <button type="submit" class='btn btn-primary'>Submit</button>
     </div>
-    <div class="col-sm-3 d-grid">
+    <div class="row justify-content-center">
         <a href="http://localhost/admin2gh/patientPage-view.php?patientId=<?php echo $patientId; ?>" class="btn btn-outline-primary" role="button">Cancel</a>
     </div>
 </div>
