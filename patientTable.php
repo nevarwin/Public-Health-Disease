248050fe40bf73f2b58d7030b1312539c4a22f67
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-06-12 00:50:39 +0800

    remove the report button

--- patientTable.php
+++ patientTable.php
@@ -20,14 +20,9 @@ include('./components/head.php');
                 ?>
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
-                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">Patients</h1>
-                        <a href="generate_report.php" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
-                    </div>
                     <?php
                     // include('./components/pageheading.php');
                     include('./components/tablePatients.php');
-
                     ?>
                 </div>
                 <!-- /.container-fluid -->
