2680a222aaf77e5c8f1508fa3dba30498df245af
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-06-20 21:42:03 +0800

    added cdns for bootstrap 4

--- components/head.php
+++ components/head.php
@@ -27,7 +27,9 @@ $user_data = check_login($con);
     <!-- chart.js cdn -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-
+    <!-- datatable cdn -->
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
 
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap4.min.css"> -->
 </head>
--- components/script.php
+++ components/script.php
@@ -11,10 +11,15 @@
     <!-- Page level plugins -->
     <script src="vendor/chart.js/Chart.min.js"></script>
 
-    <!-- Page level custom scripts -->
+    <!-- Data table CDN -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
 
+    <!-- <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap4.min.js"></script> -->
+
+    <!-- Data table Script -->
     <script>
         $(document).ready(function() {
             $('#myTable').DataTable();
