e7b63c6932bdaf939f7755dba900e65d9988ba79
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-06-30 18:13:25 +0800

    overright the datatable css

--- components/tablePatients.php
+++ components/tablePatients.php
@@ -1,3 +1,56 @@
+<style>
+    label {
+        display: flex;
+        align-items: center;
+    }
+
+    .dataTables_wrapper .dataTables_length select {
+        display: inline-block;
+        width: 100%;
+        height: calc(1.5em + 0.75rem + 2px);
+        padding: 0.375rem 1.75rem 0.375rem 0.75rem;
+        font-size: 1rem;
+        font-weight: 400;
+        line-height: 1.5;
+        color: #6e707e;
+        vertical-align: middle;
+        background: #fff url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='4' height='5' viewBox='0 0 4 5'%3e%3cpath fill='%235a5c69' d='M2 0L0 2h4zm0 5L0 3h4z'/%3e%3c/svg%3e") right 0.75rem center/8px 10px no-repeat;
+        border: 1px solid #d1d3e2;
+        border-radius: 0.35rem;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+    }
+
+    .dataTables_wrapper .dataTables_filter input:focus {
+        color: #212529;
+        background-color: #fff;
+        border-color: #86b7fe;
+        outline: 0;
+        box-shadow: 0 0 0 0.01rem rgba(13, 110, 253, 0.25);
+    }
+
+    #myTable_paginate {
+        display: flex;
+    }
+
+    .dataTables_wrapper .dataTables_paginate .paginate_button.current,
+    .dataTables_wrapper .dataTables_paginate .paginate_button.current:hover {
+        z-index: 3;
+        color: #fff !important;
+        background: #007bff !important;
+        border-color: #007bff !important;
+    }
+
+    .dataTables_wrapper .dataTables_paginate .paginate_button:hover {
+        z-index: 2;
+        color: #0056b3 !important;
+        text-decoration: none !important;
+        background: #e9ecef !important;
+        border-color: #dee2e6 !important;
+    }
+</style>
+
 <div class="card shadow mb-4">
     <div class="card-header py-3">
         <div class="row">
