2983bed8f72837191d86dff23b9e963e7a63ee1b
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-07-08 15:15:54 +0800

    make the generate button side with add patient

--- components/tablePatients.php
+++ components/tablePatients.php
@@ -56,9 +56,13 @@
         <div class="row">
             <h2 class="m-0 font-weight-bold text-primary">Patient's Data</h2>
             <!-- <input class="col-sm-3 form-control" type="text" id="searchInput" placeholder="Search"> -->
-            <a href="generate_report.php" class="btn btn-primary"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
-            <a href="http://localhost/admin2gh/patientPage-create.php" class="btn btn-primary ml-auto" role="button">Add new patient</a>
+            <div class="ml-auto">
+                <a href="generate_report.php" class="btn btn-primary"><i class="fas fa-download fa-sm text-white-50 mr-auto"></i> Generate Report</a>
+                &nbsp;
+                <a href="http://localhost/admin2gh/patientPage-create.php" class="btn btn-primary" role="button">Add new patient</a>
+            </div>
         </div>
+
     </div>
     <div class="card-body">
         <div class="table-responsive">
