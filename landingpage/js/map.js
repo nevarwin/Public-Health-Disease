fafaca3c4bf8042db9870bec18599a5d1fc00511
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-08-27 19:34:40 +0800

    improved the colors in color deficientcy

--- landingpage/js/map.js
+++ landingpage/js/map.js
@@ -1,5 +1,6 @@
 let map;
 let heatmap;
+console.log(defaultGradient);
 // var locationData;
 
 function initMap() {
@@ -18,41 +19,85 @@ function initMap() {
     ),
     map: map,
     radius: 20,
+    gradient: defaultGradient,
   });
 }
 
 const deuteranomalyColors = [
-  "rgba(0, 0, 0, 0)",
-  "rgba(255, 136, 0, 1)", // Orange
-  "rgba(0, 221, 0, 1)", // Light green
-  "rgba(0, 0, 255, 1)", // Blue
+  "rgba(255, 0, 0, 0)",
+  "rgba(255, 140, 0, 1)", // Dark Orange
+  "rgba(255, 165, 0, 1)", // Medium Orange
+  "rgba(255, 200, 100, 1)", // Light Orange
+  "rgba(144, 238, 144, 1)", // Light Green
+  "rgba(0, 192, 0, 1)", // Medium Green
+  "rgba(0, 128, 0, 1)", // Dark Green
+  "rgba(173, 216, 230, 1)", // Light Blue
+  "rgba(0, 0, 255, 1)", // Medium Blue
+  "rgba(0, 0, 128, 1)", // Dark Blue
   "rgba(0, 0, 0, 1)",
 ];
 const protanomalyColors = [
-  "rgba(0, 0, 0, 0)",
-  "rgba(255, 187, 0, 1)", // Yellow
-  "rgba(0, 238, 0, 1)", // Lime green
-  "rgba(0, 0, 255, 1)", // Blue
+  "rgba(255, 0, 0, 0)",
+  // Yellow Shades
+  "rgba(255, 255, 153, 1)", // Light Yellow
+  "rgba(255, 255, 102, 1)", // Medium Yellow
+  "rgba(255, 255, 0, 1)", // Dark Yellow
+
+  // Lime Green Shades
+  "rgba(50, 205, 50, 1)", // Dark Lime Green
+  "rgba(124, 252, 0, 1)", // Medium Lime Green
+  "rgba(144, 238, 144, 1)", // Light Lime Green
+
+  // Blue Shades
+  "rgba(173, 216, 230, 1)", // Light Blue
+  "rgba(0, 0, 255, 1)", // Medium Blue
+  "rgba(0, 0, 128, 1)", // Dark Blue
   "rgba(0, 0, 0, 1)",
 ];
 const tritanomalyColors = [
-  "rgba(0, 0, 0, 0)",
-  "rgba(255, 0, 0, 1)", // Red
-  "rgba(0, 255, 204, 1)", // Light turquoise
-  "rgba(0, 0, 255, 1)", // Blue
+  "rgba(255, 0, 0, 0)",
+  // Red Shades
+  "rgba(255, 0, 0, 1)", // Dark Red
+  "rgba(255, 69, 0, 1)", // Medium Red
+  "rgba(255, 99, 71, 1)", // Light Red
+
+  // Light Turquoise Shades
+  "rgba(64, 224, 208, 1)", // Dark Light Turquoise
+  "rgba(0, 255, 255, 1)", // Medium Light Turquoise
+  "rgba(175, 238, 238, 1)", // Light Light Turquoise
+
+  // Blue Shades
+  "rgba(173, 216, 230, 1)", // Light Blue
+  "rgba(0, 0, 255, 1)", // Medium Blue
+  "rgba(0, 0, 128, 1)", // Dark Blue
   "rgba(0, 0, 0, 1)",
 ];
-const defaultColors = [
-  "rgba(0, 0, 0, 0)",
-  "rgb(255, 0, 0)", // Red
-  "rgb(255, 255, 0)", // Yellow
-  "rgb(0, 128, 0)", // Green
-  "rgba(0, 0, 0, 1)",
-];
+// const warmColors = [
+//   "rgba(0, 0, 0, 0)",
+//   "rgb(255, 0, 0)", // Red
+//   "rgb(255, 255, 0)", // Yellow
+//   "rgb(0, 128, 0)", // Green
+//   "rgba(0, 0, 0, 1)",
+// ];
+
+// const coolors = [
+//   "rgba(0, 0, 0, 0)",
+//   "rgb(0, 102, 204)", // Blue
+//   "rgb(102, 0, 102)", // Purple
+//   "rgb(0, 204, 102)", // Teal
+//   "rgba(0, 0, 0, 1)",
+// ];
+console.log(isWarm);
+var gradientColors;
+
+var warmGradient = "red, yellow, green";
+var coolGradient = "blue, purple, teal";
+isWarm ? (gradientColors = warmGradient) : (gradientColors = coolGradient);
+document.getElementById("colorGradient").style.background =
+  "linear-gradient(to right, " + gradientColors + ")";
 
 function changeGradientColor() {
   var dropdown = document.getElementById("color-deficiency");
-  var gradientColors;
 
   if (dropdown.value === "deuteranomaly") {
     // console.log("deutera");
@@ -67,9 +112,11 @@ function changeGradientColor() {
     heatmap.set("gradient", tritanomalyColors);
     gradientColors = "#0000ff, #00ffcc, #ff0000";
   } else if (dropdown.value === "default") {
-    // console.log("trita");
-    heatmap.set("gradient", heatmap.get("gradient") ? null : defaultColors);
-    gradientColors = "red, yellow, green";
+    // console.log("default");
+    heatmap.set("gradient", defaultGradient);
+    isWarm ? (gradientColors = warmGradient) : (gradientColors = coolGradient);
+    console.log(defaultGradient);
+    console.log(isWarm);
   }
   console.log(gradientColors);
 
