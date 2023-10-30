f52e90dada76587303136fd1bfbb9b371d373601
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-10-31 01:32:48 +0800

    moved the map to the top

--- landingpage/landingpage.php
+++ landingpage/landingpage.php
@@ -106,7 +106,7 @@
   </svg>
   <!-- services section Starts -->
   <div id="scroll-target">
-    <section class="services section-padding" id="services" style="background-color: rgba(156,204,101,0.5);">
+    <section class="services section-padding pb-4" id="services" style="background-color: rgba(156,204,101,0.5);">
       <div class=" container">
         <div class="row">
           <div class="col-md-12">
@@ -118,8 +118,39 @@
             </div>
           </div>
         </div>
+        <div class="col-md-12">
+          <div class="section-header text-center pt-4">
+            <h2>Heatmap</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 col-lg-12">
+            <div class="row">
+              <div class="col col-sm-12 col-md-12 col-lg-8 section-padding pt-2" style="height: 600px;">
+                <?php include('landingPageHeatmap.php') ?>
+              </div>
+              <div class="col-sm-12 col-md-12 col-lg-4 py-5">
+                <div class="card-body">
+                  <h5 class="card-title">Cavite Health Disease Heatmap</h5>
+                  <p class="card-text" style="text-align: justify;"> A disease heatmap is a graphic that shows the severity or incidence of an illness across several geographic areas using color gradients. In general, larger illness rates or concentrations are represented by darker, warmer colors, whereas lower rates are depicted by lighter, cooler colors. The heatmap makes it simple and quick to locate hotspots with higher disease activity, which helps public health professionals better understand disease trends and carry out focused interventions.</p>
+                </div>
+                <div class="row justify-content-center pt-5">
+                  <div class="text-center">
+                    <span class=""><strong>Intensity Level</strong></span>
+                    <div class="colorGradient" id="colorGradient"></div>
+                    <div class="d-flex justify-content-between">
+                      <span class="label label-high">High</span>
+                      <span class="label label-medium">Medium</span>
+                      <span class="label label-low">Low</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <!-- Bar Chart -->
-        <div class="row my-4">
+        <div class="row">
           <div class="col-sm-12 col-md-12 col-lg-8">
             <div class="card shadow">
               <div class="card-body">
@@ -153,49 +184,11 @@
       </div>
     </section>
   </div>
-
-  <!-- services section Ends -->
-  <!-- project strats -->
-  <section id="project" class="project pb-5" style="background-color: rgba(156,204,101,0.5);">
-    <div class="container">
-      <div class="col-md-12">
-        <div class="section-header text-center">
-          <h2>Heatmap</h2>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-12 col-lg-12">
-          <div class="row">
-            <div class="col col-sm-12 col-md-12 col-lg-8" style="height: 600px;">
-              <?php include('landingPageHeatmap.php') ?>
-            </div>
-            <div class="col-sm-12 col-md-12 col-lg-4 py-5">
-              <div class="card-body">
-                <h5 class="card-title">Cavite Health Disease Heatmap</h5>
-                <p class="card-text" style="text-align: justify;"> A disease heatmap is a graphic that shows the severity or incidence of an illness across several geographic areas using color gradients. In general, larger illness rates or concentrations are represented by darker, warmer colors, whereas lower rates are depicted by lighter, cooler colors. The heatmap makes it simple and quick to locate hotspots with higher disease activity, which helps public health professionals better understand disease trends and carry out focused interventions.</p>
-              </div>
-              <div class="row justify-content-center pt-5">
-                <div class="text-center">
-                  <span class=""><strong>Intensity Level</strong></span>
-                  <div class="colorGradient" id="colorGradient"></div>
-                  <div class="d-flex justify-content-between">
-                    <span class="label label-high">High</span>
-                    <span class="label label-medium">Medium</span>
-                    <span class="label label-low">Low</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
     <path fill="#9CCC65" fill-opacity="0.5" d="M0,256L14.1,250.7C28.2,245,56,235,85,192C112.9,149,141,75,169,80C197.6,85,226,171,254,202.7C282.4,235,311,213,339,176C367.1,139,395,85,424,53.3C451.8,21,480,11,508,42.7C536.5,75,565,149,593,170.7C621.2,192,649,160,678,165.3C705.9,171,734,213,762,240C790.6,267,819,277,847,256C875.3,235,904,181,932,160C960,139,988,149,1016,144C1044.7,139,1073,117,1101,101.3C1129.4,85,1158,75,1186,85.3C1214.1,96,1242,128,1271,170.7C1298.8,213,1327,267,1355,277.3C1383.5,288,1412,256,1426,240L1440,224L1440,0L1425.9,0C1411.8,0,1384,0,1355,0C1327.1,0,1299,0,1271,0C1242.4,0,1214,0,1186,0C1157.6,0,1129,0,1101,0C1072.9,0,1045,0,1016,0C988.2,0,960,0,932,0C903.5,0,875,0,847,0C818.8,0,791,0,762,0C734.1,0,706,0,678,0C649.4,0,621,0,593,0C564.7,0,536,0,508,0C480,0,452,0,424,0C395.3,0,367,0,339,0C310.6,0,282,0,254,0C225.9,0,198,0,169,0C141.2,0,113,0,85,0C56.5,0,28,0,14,0L0,0Z"></path>
   </svg>
   <!-- about section starts -->
-  <section id="about" class="about section-padding">
+  <section id="about" class="about section-padding pt-0">
     <div class="container">
       <div class="row">
         <div class="col-md-12">
@@ -212,9 +205,9 @@
         </div> -->
         <div class="col-lg-12 col-md-12 col-12 ps-lg-5">
           <div class="about-text">
-            <p>
+            <h5>
               Our Public Health Disease Geomapping website is a detailed online resource made to offer engaging and educational representations of disease data on maps of Cavite. For information on the geographic distribution of diseases and their effects on various locations, public health authorities, researchers, and the general public can all benefit from visiting our website.
-            </p>
+            </h5>
           </div>
         </div>
       </div>
