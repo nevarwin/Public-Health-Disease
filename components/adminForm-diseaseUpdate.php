af80d2946585cbe252a5fee980b99fa5ac6e4ddc
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-11-09 01:22:37 -0600

    Refactor disease update form and add functionality to edit and add diseases

--- components/adminForm-diseaseUpdate.php
+++ components/adminForm-diseaseUpdate.php
@@ -1,68 +1,82 @@
-<?php
-include('connection.php');
-include('alertMessage.php');
+<body>
+    <div class="container mt-4">
+        <h1 class="text-center">List of Diseases</h1>
 
-$message = '';
-$type = '';
-$strongContent = '';
-
-// Get admin by id
-if ($user_data['positionId'] != 1) {
-    $link = "http://localhost/admin2gh/patientTable.php";
-} else {
-    $link = "http://localhost/admin2gh/adminTable.php";
-}
-
-// Check if the 'id' key is present in the GET request
-
-// Fetch the disease from the database
-$sql = "SELECT * FROM diseases WHERE id = $diseaseId";
-$result = mysqli_query($con, $sql);
-
-if (!$result) {
-    echo 'Error in fetching disease: ' . mysqli_error($con);
-    exit;
-}
-
-// Check if the disease record was found
-if ($row = mysqli_fetch_assoc($result)) {
-    $diseaseName = $row['disease'];
-} else {
-    echo 'Disease not found';
-    exit;
-}
-
-?>
-
-<div class="row d-flex justify-content-center">
-    <div class="card shadow col-md-12 col-sm-4 col-lg-6" style="padding: 30px">
-        <h2 class="row justify-content-center mb-3">Edit Diseases:</h2>
-        <?php
-        if (!empty($alert)) {
-            echo $alert;
-        }
-        ?>
+        <!-- Add Disease Section -->
+        <div class="mb-4">
+            <h2>Add New Disease</h2>
+            <form class="form-inline" method="post" action="">
+                <div class="form-group">
+                    <input type="text" class="form-control mr-2" name="newDiseaseName" placeholder="New Disease Name">
+                    <button type="submit" class="btn btn-primary" name="add_disease">Add Disease</button>
+                </div>
+            </form>
+        </div>
 
         <table class="table table-bordered">
             <thead>
                 <tr>
-                    <th>ID</th>
+                    <th>Disease ID</th>
                     <th>Disease Name</th>
-                    <th>Action</th>
+                    <th>Edit</th>
                 </tr>
             </thead>
             <tbody>
                 <?php
-                while ($row = mysqli_fetch_assoc($result)) {
-                    echo "<tr>";
-                    echo "<td>{$row['id']}</td>";
-                    echo "<td>{$row['disease']}</td>";
-                    echo "<td><a href='edit_disease.php?id={$row['id']}' class='btn btn-primary'>Edit</a></td>";
-                    echo "</tr>";
+                include('connection.php');
+
+                // Check if a form was submitted for editing
+                if (isset($_POST['edit_disease'])) {
+                    $diseaseId = $_POST['diseaseId'];
+                    $newDiseaseName = $_POST['newDiseaseName'];
+
+                    // Update the disease name in the database
+                    $updateSql = "UPDATE diseases SET disease = '$newDiseaseName' WHERE diseaseId = $diseaseId";
+                    if ($con->query($updateSql) === TRUE) {
+                        echo "Disease updated successfully.";
+                    } else {
+                        echo "Error updating disease: " . $con->error;
+                    }
+                }
+
+                // Check if a form was submitted for adding a new disease
+                if (isset($_POST['add_disease'])) {
+                    $newDiseaseName = $_POST['newDiseaseName'];
+
+                    // Insert the new disease into the database
+                    $insertSql = "INSERT INTO diseases (disease) VALUES ('$newDiseaseName')";
+                    if ($con->query($insertSql) === TRUE) {
+                        echo "Disease added successfully.";
+                    } else {
+                        echo "Error adding disease: " . $con->error;
+                    }
+                }
+
+                // SQL query to fetch diseases
+                $sql = "SELECT * FROM diseases";
+
+                $result = $con->query($sql);
+
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>" . $row["diseaseId"] . "</td>";
+                        echo "<td>" . $row["disease"] . "</td>";
+                        echo "<td>
+                            <form class='form-inline' method='post' action=''>
+                                <div class='form group'>
+                                    <input class='form-control' type='hidden' name='diseaseId' value='" . $row["diseaseId"] . "'>
+                                    <input class='form-control' type='text' name='newDiseaseName' placeholder='New Disease Name'>
+                                    <input type='submit' class='btn btn-primary' name='edit_disease' value='Edit'>
+                                </div>
+                            </form>
+                        </td>";
+                        echo "</tr>";
+                    }
+                } else {
+                    echo "No diseases found in the database.";
                 }
                 ?>
             </tbody>
         </table>
-
     </div>
-</div>
