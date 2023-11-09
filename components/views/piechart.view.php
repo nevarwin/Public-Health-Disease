1f3eafaccd0caad5d36056cc6a427a79a9306547
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-11-09 01:23:01 -0600

    Refactored piechart dropdown to fetch data from database

--- components/views/piechart.view.php
+++ components/views/piechart.view.php
@@ -10,29 +10,17 @@ if (!empty($errorMessage)) {
                 <label for="disease">Select Disease:</label>
                 <select class="custom-select" name="pieDisease">
                     <?php
-                    $pieDropdown = [
-                        1 => 'ABD',
-                        2 => 'AEFI',
-                        3 => 'AES',
-                        4 => 'AFP',
-                        5 => 'AMES',
-                        6 => 'ChikV',
-                        7 => 'DIPH',
-                        8 => 'HFMD',
-                        9 => 'NNT',
-                        10 => 'NT',
-                        11 => 'PERT',
-                        12 => 'Influenza',
-                        13 => 'Dengue',
-                        14 => 'Rabies',
-                        15 => 'Cholera',
-                        16 => 'Hepatitis',
-                        17 => 'Measles',
-                        18 => 'Meningitis',
-                        19 => 'Meningo',
-                        20 => 'Typhoid',
-                        21 => 'Leptospirosis',
-                    ];
+                    $sql = "SELECT diseaseId, disease FROM diseases";
+                    $result = $con->query($sql);
+
+                    $pieDropdown = [];
+
+                    if ($result->num_rows > 0) {
+                        while ($row = $result->fetch_assoc()) {
+                            $pieDropdown[$row['diseaseId']] = $row['disease'];
+                        }
+                    }
+
                     $pieSelectedDisease = $_GET['pieDisease'] ?? '';
 
                     foreach ($pieDropdown as $key => $value) {
