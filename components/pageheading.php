570e367c0a503ca7f0e6761ba9092204ad49371d
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-11-24 05:52:10 -0600

    Delete unnecessary files and code

--- components/pageheading.php
+++
@@ -1,5 +0,0 @@
-<!-- Page Heading -->
-
-
-<a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
-</div>
--- generate.php
+++
@@ -1,4 +0,0 @@
-<?php
-require '1Header.php';
-include('./components/generateview.php');
-require '1Footer.php';
