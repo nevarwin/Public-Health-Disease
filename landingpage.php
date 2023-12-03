10c0c19f5751d95481cb2f78c8906f39d9939b7f
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-12-03 15:16:42 +0800

    copied the landing page to the root directory

---
+++ landingpage.php
@@ -0,0 +1,348 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Public Health Disease Geomapping for Cavite</title>
+  <link rel="shortcut icon" href="assets/img/caviteLogo.png" type="image/png">
+
+  <!-- chart.js cdn -->
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+  <!-- All CSS -->
+  <link href="landingpage/css/bootstrap.min.css" rel="stylesheet" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" />
+  <link rel="stylesheet" href="landingpage/css/style.css" />
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" style='color:white' ; href="#"><span class="text-success">Public </span>Health <span class="text-warning">Disease </span>Geomapping for Cavite</a>
+    </div>
+  </nav>
+
+  <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <<div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="landingpage/img/phhome1.jpg" class="d-block w-100" alt="Public Health Disease Geomapping" />
+        <div class="carousel-caption">
+          <h5>Stay Informed</h5>
+          <p>
+            Get the latest updates on disease geomapping and its impact on communities.
+          </p>
+          <p><a href="#geomapping" class="btn btn-warning mt-3">Learn More</a></p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="landingpage/img/phhome3.jpg" class="d-block w-100" alt="Public Health Disease Geomapping" />
+        <div class="carousel-caption">
+          <h5>Empowering Communities</h5>
+          <p>
+            Explore how disease geomapping empowers communities in preventing and addressing public health challenges.
+          </p>
+          <p><a href="#services" class="btn btn-warning mt-3">Learn More</a></p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="landingpage/img/phhome2.jpg" class="d-block w-100" alt="Public Health Disease Geomapping" />
+        <div class="carousel-caption">
+          <h5>Dedicated to Public Health</h5>
+          <p>
+            Discover our commitment to ensuring public health through effective disease geomapping initiatives.
+          </p>
+          <p><a href="#about" class="btn btn-warning mt-3">Learn More</a></p>
+        </div>
+      </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+  </div>
+  <!-- about section starts -->
+  <section id="geomapping" class="about pt-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="section-header text-center pb-5">
+            <h2>What is Geomapping?</h2>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-6 col-md-12 col-12">
+          <div class="about-img">
+            <img src="landingpage/img/geomapping.svg" alt="" class="img-fluid" />
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-12 col-12 ps-lg-5 mt-md-5">
+          <div class="about-text">
+            <h5>
+              Geomapping is a powerful tool that can be used to track and control diseases. By overlaying data on a map, public health officials can identify areas where diseases are most prevalent and take steps to prevent the spread of disease.
+              <br>
+              <br>
+              For example, geomapping can be used to track the spread of influenza. This information can be used to warn residents about areas where the virus is present and to take steps to prevent the spread of the virus.
+            </h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- about section Ends -->
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319">
+    <path fill="#9CCC65" fill-opacity="0.5" d="M0,160L36.9,160L73.8,160L110.8,32L147.7,288L184.6,224L221.5,160L258.5,224L295.4,256L332.3,192L369.2,288L406.2,192L443.1,256L480,288L516.9,128L553.8,160L590.8,288L627.7,128L664.6,224L701.5,160L738.5,128L775.4,224L812.3,128L849.2,32L886.2,32L923.1,288L960,224L996.9,32L1033.8,96L1070.8,64L1107.7,192L1144.6,320L1181.5,256L1218.5,192L1255.4,192L1292.3,224L1329.2,160L1366.2,224L1403.1,256L1440,192L1440,320L1403.1,320L1366.2,320L1329.2,320L1292.3,320L1255.4,320L1218.5,320L1181.5,320L1144.6,320L1107.7,320L1070.8,320L1033.8,320L996.9,320L960,320L923.1,320L886.2,320L849.2,320L812.3,320L775.4,320L738.5,320L701.5,320L664.6,320L627.7,320L590.8,320L553.8,320L516.9,320L480,320L443.1,320L406.2,320L369.2,320L332.3,320L295.4,320L258.5,320L221.5,320L184.6,320L147.7,320L110.8,320L73.8,320L36.9,320L0,320Z"></path>
+  </svg>
+  <!-- services section Starts -->
+  <div id="scroll-target">
+    <section class="services py-4" id="services" style="background-color: rgba(156,204,101,0.5);">
+      <div class=" container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="section-header text-center">
+              <h2>Data Visualizations</h2>
+              <?php
+              include('landingpage/js/landingDropdown.php')
+              ?>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 col-lg-12">
+            <div class="row">
+              <div class="col col-sm-12 col-md-12 col-lg-8 section-padding pt-2" style="height: 600px;">
+                <?php include('landingpage/landingPageHeatmap.php') ?>
+              </div>
+              <div class="col-sm-12 col-md-12 col-lg-4 py-5">
+                <div class="card-body">
+                  <h5 class="card-title">Disease Heatmap: Visualizing Geographic Disease Incidence</h5>
+                  <p class="card-text" style="text-align: justify;"> A disease heatmap is a visual representation of illness severity or incidence across geographic areas, using color gradients. Darker, warmer colors indicate higher rates, while lighter, cooler colors represent lower rates. This visualization helps identify disease hotspots, aiding public health professionals in understanding and targeting disease trends</p>
+                </div>
+                <div class="row justify-content-center pt-5">
+                  <div class="text-center">
+                    <span class=""><strong>Intensity Level</strong></span>
+                    <div class="colorGradient" id="colorGradient"></div>
+                    <div class="d-flex justify-content-between">
+                      <span class="label label-high">High</span>
+                      <span class="label label-medium">Medium</span>
+                      <span class="label label-low">Low</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Bar Chart -->
+        <div class="row">
+          <div class="col-sm-12 col-md-12 col-lg-8">
+            <div class="card shadow">
+              <div class="card-body">
+                <canvas id="myChart"></canvas>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-12 col-lg-4 col-xl-4">
+            <div class="card-body text-center">
+              <h5 class="card-title">Disease count per year chart</h5>
+              <p class="card-text" style="text-align: justify;">This chart displays the annual count of a specific disease, making it easy to track changes and trends in disease prevalence over time.</p>
+            </div>
+          </div>
+        </div>
+
+        <!-- Age Bar Chart -->
+        <div class="row mt-2">
+          <div class="col-sm-12 col-md-12 col-lg-8">
+            <div class="card shadow">
+              <div class="card-body">
+                <canvas id="ageChart"></canvas>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-12 col-lg-4 col-xl-4">
+            <div class="card-body text-center">
+              <h5 class="card-title">Age distribution of disease</h5>
+              <p class="card-text" style="text-align: justify;">This chart shows how a disease is distributed among different age groups, helping to identify trends and at-risk age categories.</p>
+            </div>
+          </div>
+        </div>
+
+        <!-- Pie Chart -->
+        <div class="row mt-2">
+          <div class="col-sm-12 col-md-12 col-lg-8">
+            <div class="card shadow">
+              <div class="card-body">
+                <canvas id="pieChart"></canvas>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-12 col-lg-4 col-xl-4">
+            <div class="card-body text-center">
+              <h5 class="card-title">Municipality-wise Disease Case Distribution</h5>
+              <p class="card-text" style="text-align: justify;">This pie chart depicts disease cases across municipalities. Each segment represents a different municipality, with larger segments indicating higher disease cases. It facilitates regional disease comparisons and highlights areas with varying disease prevalence.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+  <svg class="m-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+    <path fill="#9CCC65" fill-opacity="0.5" d="M0,256L14.1,250.7C28.2,245,56,235,85,192C112.9,149,141,75,169,80C197.6,85,226,171,254,202.7C282.4,235,311,213,339,176C367.1,139,395,85,424,53.3C451.8,21,480,11,508,42.7C536.5,75,565,149,593,170.7C621.2,192,649,160,678,165.3C705.9,171,734,213,762,240C790.6,267,819,277,847,256C875.3,235,904,181,932,160C960,139,988,149,1016,144C1044.7,139,1073,117,1101,101.3C1129.4,85,1158,75,1186,85.3C1214.1,96,1242,128,1271,170.7C1298.8,213,1327,267,1355,277.3C1383.5,288,1412,256,1426,240L1440,224L1440,0L1425.9,0C1411.8,0,1384,0,1355,0C1327.1,0,1299,0,1271,0C1242.4,0,1214,0,1186,0C1157.6,0,1129,0,1101,0C1072.9,0,1045,0,1016,0C988.2,0,960,0,932,0C903.5,0,875,0,847,0C818.8,0,791,0,762,0C734.1,0,706,0,678,0C649.4,0,621,0,593,0C564.7,0,536,0,508,0C480,0,452,0,424,0C395.3,0,367,0,339,0C310.6,0,282,0,254,0C225.9,0,198,0,169,0C141.2,0,113,0,85,0C56.5,0,28,0,14,0L0,0Z"></path>
+  </svg>
+  <!-- about section starts -->
+  <section id="about" class="about section-padding pt-0">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="section-header text-center">
+            <h2>About Us</h2>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <!-- <div class="col-lg-6 col-md-12 col-12">
+          <div class="about-img">
+            <img src="img/phabout.jpg" alt="" class="img-fluid" />
+          </div>
+        </div> -->
+        <div class="col-lg-12 col-md-12 col-12 ps-lg-5">
+          <div class="about-text">
+            <h5>
+              Our Public Health Disease Geomapping website is a detailed online resource made to offer engaging and educational representations of disease data on maps of Cavite. For information on the geographic distribution of diseases and their effects on various locations, public health authorities, researchers, and the general public can all benefit from visiting our website.
+            </h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div class="accordion" id="municipalityAccordion">
+    <div class="card">
+      <div class="card-header" id="headingFour">
+        <h2 class="mb-0 text-center"> <!-- Center the heading -->
+          <button class="btn btn-link text-decoration-none" type="button" data-bs-toggle="collapse" data-bs-target="#municipalityCollapse" aria-expanded="true" aria-controls="municipalityCollapse">
+            Municipality Cases Per Disease on Current Year
+          </button>
+        </h2>
+      </div>
+
+      <div id="municipalityCollapse" class="collapse" aria-labelledby="headingFour" data-parent="#municipalityAccordion">
+        <div class="card-body">
+          <div class="row">
+            <?php
+            // Define the list of diseases with abbreviations
+            $diseasesAbbreviations = array(
+              'ABD' => 'Amoebiasis',
+              'AEFI' => 'Adverse Event Following Immunization',
+              'AES' => 'Acute encephalitis syndrome',
+              'AFP' => 'Alpha-Fetoprotein',
+              'AMES' => 'Acute Meningitis',
+              'ChikV' => 'Chikungunya Virus',
+              'DIPH' => 'Diphtheria',
+              'HFMD' => 'Hand, Foot, and Mouth Disease',
+              'NNT' => 'Number Needed to Treat',
+              'NT' => 'Neonatal Tetanus',
+              'PERT' => 'Perthes Disease',
+              'Influenza' => 'Influenza',
+              'Dengue' => 'Dengue',
+              'Rabies' => 'Rabies',
+              'Cholera' => 'Cholera',
+              'Hepatitis' => 'Hepatitis',
+              'Measles' => 'Measles',
+              'Meningitis' => 'Meningitis',
+              'Meningo' => 'Meningo',
+              'Typhoid' => 'Typhoid',
+              'Leptospirosis' => 'Leptospirosis'
+            );
+
+            $currentYear = date('Y');
+
+            $query = "SELECT COUNT(p.patientId) AS count, m.municipality, d.disease
+                      FROM patients p
+                      INNER JOIN municipality m ON p.municipality = m.munId
+                      INNER JOIN diseases d ON p.disease = d.diseaseId
+                      WHERE YEAR(p.creationDate) = '$currentYear'
+                      AND d.disease IN ('" . implode("','", array_keys($diseasesAbbreviations)) . "')
+                      GROUP BY p.municipality, m.municipality, d.disease
+                      ORDER BY m.municipality, count DESC";
+
+            $result = mysqli_query($con, $query);
+
+            $previousMunicipality = null;
+
+            // Group diseases by municipality
+            $municipalities = array();
+
+            // Collect the top 4 diseases per municipality
+            while ($row = mysqli_fetch_assoc($result)) {
+              $municipality = $row['municipality'];
+              $diseaseAbbreviation = $row['disease'];
+              $count = $row['count'];
+
+              if (!array_key_exists($municipality, $municipalities)) {
+                $municipalities[$municipality] = array();
+              }
+
+              if (count($municipalities[$municipality]) < 4) {
+                // Use the disease mapping to get the full disease name
+                $disease = $diseasesAbbreviations[$diseaseAbbreviation];
+                $municipalities[$municipality][] = array("disease" => $disease, "count" => $count);
+              }
+            }
+
+            // Display the cases per municipality
+            foreach ($municipalities as $municipality => $diseases) {
+            ?>
+              <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
+                <div class="card border border-warning shadow mb-3">
+                  <div class="card-body">
+                    <div class="row align-items-center">
+                      <div class="col">
+                        <h6 class="text-warning text-uppercase mb-2"><?= $municipality ?></h6>
+                        <?php foreach ($diseases as $diseaseData) : ?>
+                          <?php $disease = $diseaseData["disease"]; ?>
+                          <?php $count = $diseaseData["count"]; ?>
+                          <p class="mb-2"><?= $disease . ': ' . $count ?></p>
+                        <?php endforeach; ?>
+                      </div>
+                      <div class="col-auto">
+                        <i class="fas fa-disease fa-2x text-gray-300"></i>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            <?php
+            }
+            ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- about section Ends -->
+  <!-- footer starts -->
+  <footer class="bg-dark p-2 text-center">
+    <div class="container">
+      <p class="text-white">Copyright © Public Health Disease Geo Mapping 2023</p>
+    </div>
+  </footer>
+  <!-- footer ends -->
+
+  <!-- All Js -->
+  <script src="landingpage/js/landingPageChart.js"></script>
+  <script src="landingpage/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
